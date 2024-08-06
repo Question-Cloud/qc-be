32ba2233a8c893f93d6579705fe026f43be6ed73
e4g3r <qaws5656@naver.com>
2024-08-06 16:53:16 +0900

    init

---
+++ src/main/java/com/eager/questioncloud/QuestioncloudApplication.java
@@ -0,0 +1,13 @@
+package com.eager.questioncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestioncloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestioncloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eager/questioncloud/QuestioncloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eager.questioncloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestioncloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
