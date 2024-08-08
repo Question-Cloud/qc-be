0e0f491729c3874465a7a1863537518c4626a663
e4g3r <qaws5656@naver.com>
2024-08-08 16:47:56 +0900

    refactor: CreateUserService -> UserService

--- src/main/java/com/eager/questioncloud/user/CreateUserController.java
+++ src/main/java/com/eager/questioncloud/user/CreateUserController.java
@@ -2,10 +2,7 @@ package com.eager.questioncloud.user;
 
 import com.eager.questioncloud.user.Request.CreateUserRequest;
 import jakarta.validation.Valid;
-import java.net.http.HttpResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class CreateUserController {
-    private final CreateUserService createUserService;
+    private final UserService userService;
 
     @PostMapping
     public void createUser(@RequestBody @Valid CreateUserRequest createUserRequest) {
         createUserRequest.validate();
-        createUserService.create(User.create(createUserRequest));
+        userService.create(User.create(createUserRequest));
     }
 }
