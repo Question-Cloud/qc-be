6137b3e00a15948ecbb2cdcdb8e5ec60b3edea40
e4g3r <qaws5656@naver.com>
2024-08-08 16:47:32 +0900

    refactor: CreateUserService -> UserService

--- src/main/java/com/eager/questioncloud/user/UserService.java
+++ src/main/java/com/eager/questioncloud/user/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CreateUserService {
+public class UserService {
     private final UserCreator userCreator;
 
     public User create(User user) {
