641e80d6576da34737f587d5a7090fa393661cbf
e4g3r <qaws5656@naver.com>
2024-08-13 23:10:43 +0900

    feat: Google 로그인 / 회원가입 로직

---
+++ src/main/java/com/eager/questioncloud/social/GoogleUserInfo.java
@@ -0,0 +1,11 @@
+package com.eager.questioncloud.social;
+
+import lombok.Getter;
+
+@Getter
+public class GoogleUserInfo {
+    private String id;
+    private String email;
+    private String name;
+    private String picture;
+}
