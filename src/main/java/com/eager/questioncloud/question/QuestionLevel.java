9e496bf696ebae068994df5442ca927e78873091
e4g3r <qaws5656@naver.com>
2024-08-15 20:32:36 +0900

    feat: QuestionLevel 속성 2개 추가

--- src/main/java/com/eager/questioncloud/question/QuestionLevel.java
+++ src/main/java/com/eager/questioncloud/question/QuestionLevel.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum QuestionLevel {
-    LEVEL1("LEVEL1"), LEVEL2("LEVEL2"), LEVEL3("LEVEL3"), LEVEL4("LEVEL4");
+    LEVEL1("LEVEL1"), LEVEL2("LEVEL2"), LEVEL3("LEVEL3"), LEVEL4("LEVEL4"), LEVEL5("LEVEL5"), LEVEL6("LEVEL6");
     private final String value;
 
     QuestionLevel(String value) {
