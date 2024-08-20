0efda883f7a78535408776eab8ddc69ac05712fd
e4g3r <qaws5656@naver.com>
2024-08-20 14:57:30 +0900

    refactor: couponId -> userCouponId

--- src/main/java/com/eager/questioncloud/payment/QuestionPaymentProcessor.java
+++ src/main/java/com/eager/questioncloud/payment/QuestionPaymentProcessor.java
@@ -21,13 +21,13 @@ public class QuestionPaymentProcessor {
     private final UserCouponProcessor userCouponProcessor;
 
     @Transactional
-    public QuestionPayment questionPayment(Long userId, List<Long> questionIds, Long couponId) {
+    public QuestionPayment questionPayment(Long userId, List<Long> questionIds, Long userCouponId) {
         int originalAmount = getOriginalAmount(questionIds);
-        int finalAmount = isUsingCoupon(couponId) ? userCouponProcessor.useCoupon(userId, couponId, originalAmount) : originalAmount;
-        
+        int finalAmount = isUsingCoupon(userCouponId) ? userCouponProcessor.useCoupon(userId, userCouponId, originalAmount) : originalAmount;
+
         userPointManager.usePoint(userId, finalAmount);
 
-        QuestionPayment questionPayment = questionPaymentCreator.createQuestionPayment(QuestionPayment.create(userId, couponId, finalAmount));
+        QuestionPayment questionPayment = questionPaymentCreator.createQuestionPayment(QuestionPayment.create(userId, userCouponId, finalAmount));
         questionPaymentOrderCreator.createQuestionPaymentOrders(QuestionPaymentOrder.createOrders(questionPayment.getId(), questionIds));
 
         userQuestionLibraryCreator.appendUserQuestion(userId, questionIds);
