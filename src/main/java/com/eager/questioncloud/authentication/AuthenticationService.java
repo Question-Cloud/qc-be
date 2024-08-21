fae43ff9538867a9a39d36277cac0f03c08739ee
e4g3r <qaws5656@naver.com>
2024-08-22 00:59:53 +0900

    feat: Spring Security GUEST 인증

--- src/main/java/com/eager/questioncloud/authentication/AuthenticationService.java
+++ src/main/java/com/eager/questioncloud/authentication/AuthenticationService.java
@@ -42,4 +42,8 @@ public class AuthenticationService {
         } catch (Exception ignored) {
         }
     }
+
+    public void guestAuthentication() {
+        authenticationManager.setGuest();
+    }
 }
