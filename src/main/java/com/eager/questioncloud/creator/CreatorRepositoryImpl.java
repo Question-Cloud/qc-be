86aac2e6df1a01426f9040d8c04a56e3511cd59a
e4g3r <qaws5656@naver.com>
2024-08-27 20:51:05 +0900

    feat: Creator 도메인 초안

---
+++ src/main/java/com/eager/questioncloud/creator/CreatorRepositoryImpl.java
@@ -0,0 +1,9 @@
+package com.eager.questioncloud.creator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CreatorRepositoryImpl implements CreatorRepository {
+}
