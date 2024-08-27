5b0e3a0549d4be74bc371f92f0822b4b514ff40f
e4g3r <qaws5656@naver.com>
2024-08-27 20:14:10 +0900

    feat: 문제 Creator 도메인 초안

---
+++ src/main/java/com/eager/questioncloud/creator/CreatorJpaRepository.java
@@ -0,0 +1,6 @@
+package com.eager.questioncloud.creator;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreatorJpaRepository extends JpaRepository<CreatorEntity, Long> {
+}
---
+++ src/main/java/com/eager/questioncloud/creator/CreatorRepository.java
@@ -0,0 +1,4 @@
+package com.eager.questioncloud.creator;
+
+public interface CreatorRepository {
+}
