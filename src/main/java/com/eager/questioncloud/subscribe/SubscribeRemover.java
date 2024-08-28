08ca0410963f5cdc624695d8f57b5e0d206fde6e
e4g3r <qaws5656@naver.com>
2024-08-28 23:27:58 +0900

    fix: unSubscribe 함수 @Transactional 추가

--- src/main/java/com/eager/questioncloud/subscribe/SubscribeRemover.java
+++ src/main/java/com/eager/questioncloud/subscribe/SubscribeRemover.java
@@ -1,5 +1,6 @@
 package com.eager.questioncloud.subscribe;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class SubscribeRemover {
     private final SubscribeRepository subscribeRepository;
 
+    @Transactional
     public void unSubscribe(Long userId, Long creatorId) {
         subscribeRepository.unSubscribe(userId, creatorId);
     }
