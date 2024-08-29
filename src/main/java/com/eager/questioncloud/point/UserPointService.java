ec120d228e5fd02879211eb2fed0a79103ae7417
e4g3r <qaws5656@naver.com>
2024-08-29 14:45:19 +0900

    refactor: 포인트 충전 / 사용 로직 구현체 클래스 통합  - UserPointCharger 충전 로직 UserPointProcessor로 이동

--- src/main/java/com/eager/questioncloud/point/UserPointService.java
+++ src/main/java/com/eager/questioncloud/point/UserPointService.java
@@ -7,13 +7,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserPointService {
     private final UserPointReader userPointReader;
-    private final UserPointCharger userPointCharger;
+    private final UserPointProcessor userPointProcessor;
 
     public int getUserPoint(Long userId) {
         return userPointReader.getUserPoint(userId);
     }
 
     public void chargePoint(Long userId, ChargePointType chargePointType, String paymentId) {
-        userPointCharger.chargePoint(userId, chargePointType, paymentId);
+        userPointProcessor.chargePoint(userId, chargePointType, paymentId);
     }
 }
