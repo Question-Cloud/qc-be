82f1161895d3f6afe37aed72837387dd1e0a82ac
e4g3r <qaws5656@naver.com>
2024-09-07 22:25:07 +0900

    refactor: 문제 후기 조회 API 파라미터 방식 수정  - questionId를 url parameter로 받도록 수정

--- src/main/java/com/eager/questioncloud/question/QuestionReviewController.java
+++ src/main/java/com/eager/questioncloud/question/QuestionReviewController.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,9 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionReviewController {
     private final QuestionReviewService questionReviewService;
 
-    @GetMapping("/{questionId}")
+    @GetMapping
     public PagingResponse<QuestionReviewItem> getQuestionReviews(
-        @AuthenticationPrincipal UserPrincipal userPrincipal, @PathVariable Long questionId, Pageable pageable) {
+        @AuthenticationPrincipal UserPrincipal userPrincipal, @RequestParam Long questionId, Pageable pageable) {
         int total = questionReviewService.getTotal(questionId);
         List<QuestionReviewItem> questionReviewItems = questionReviewService.getQuestionReviews(
             questionId,
