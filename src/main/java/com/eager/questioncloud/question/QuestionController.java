92be64de09c4d56cd7e755e66d1b23e91dde9a61
e4g3r <qaws5656@naver.com>
2024-09-07 20:58:16 +0900

    feat: QuestionFilter Resolver 적용

--- src/main/java/com/eager/questioncloud/question/QuestionController.java
+++ src/main/java/com/eager/questioncloud/question/QuestionController.java
@@ -6,12 +6,10 @@ import com.eager.questioncloud.question.Response.QuestionInformationResponse;
 import com.eager.questioncloud.security.UserPrincipal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,16 +19,7 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping
-    public PagingResponse<QuestionInformation> getQuestionListByFiltering(
-        @AuthenticationPrincipal UserPrincipal userPrincipal,
-        @RequestParam(required = false) List<Long> categories,
-        @RequestParam(required = false) List<QuestionLevel> levels,
-        @RequestParam(required = false) QuestionType questionType,
-        @RequestParam(required = false) Long creatorId,
-        @RequestParam QuestionSortType sort,
-        Pageable pageable) {
-        QuestionFilter questionFilter = new QuestionFilter(
-            userPrincipal.getUser().getUid(), categories, levels, questionType, creatorId, sort, pageable);
+    public PagingResponse<QuestionInformation> getQuestionListByFiltering(QuestionFilter questionFilter) {
         int total = questionService.getTotalFiltering(questionFilter);
         List<QuestionInformation> questionInformation = questionService.getQuestionListByFiltering(questionFilter);
         return new PagingResponse<>(total, questionInformation);
