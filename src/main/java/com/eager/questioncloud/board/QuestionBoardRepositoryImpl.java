c6261a21dd9536d7681af4522346fb6efd44ad5e
e4g3r <qaws5656@naver.com>
2024-09-08 23:33:47 +0900

    fix: QuestionBoardRepositoryImpl implements 처리

--- src/main/java/com/eager/questioncloud/board/QuestionBoardRepositoryImpl.java
+++ src/main/java/com/eager/questioncloud/board/QuestionBoardRepositoryImpl.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class QuestionBoardRepositoryImpl {
+public class QuestionBoardRepositoryImpl implements QuestionBoardRepository {
 }
