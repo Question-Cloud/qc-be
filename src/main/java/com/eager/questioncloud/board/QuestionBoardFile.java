ecd7f9b688ddd28643b789ec1e2f0c70b475ef8c
e4g3r <qaws5656@naver.com>
2024-09-09 15:28:08 +0900

    fix: Converter를 위한 기본 생성자 추가

--- src/main/java/com/eager/questioncloud/board/QuestionBoardFile.java
+++ src/main/java/com/eager/questioncloud/board/QuestionBoardFile.java
@@ -2,9 +2,11 @@ package com.eager.questioncloud.board;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class QuestionBoardFile {
     private String fileName;
     private String url;
