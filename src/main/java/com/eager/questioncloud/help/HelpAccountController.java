f345d998ea74188bc07d92f4653ddbcc6f46845f
e4g3r <qaws5656@naver.com>
2024-09-14 19:56:49 +0900

    refactor: 계정 찾기 핸드폰 번호 url 파라미터 방식으로 수정

--- src/main/java/com/eager/questioncloud/help/HelpAccountController.java
+++ src/main/java/com/eager/questioncloud/help/HelpAccountController.java
@@ -1,11 +1,9 @@
 package com.eager.questioncloud.help;
 
 import com.eager.questioncloud.common.DefaultResponse;
-import com.eager.questioncloud.help.Request.RecoverForgottenEmailRequest;
 import com.eager.questioncloud.help.Response.RecoverForgottenEmailResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +15,8 @@ public class HelpAccountController {
     private final HelpAccountService helpAccountService;
 
     @GetMapping("/recover/forgotten-email")
-    public RecoverForgottenEmailResponse recoverForgottenEmail(@RequestBody RecoverForgottenEmailRequest request) {
-        String email = helpAccountService.recoverForgottenEmail(request.getPhone());
+    public RecoverForgottenEmailResponse recoverForgottenEmail(@RequestParam String phone) {
+        String email = helpAccountService.recoverForgottenEmail(phone);
         return new RecoverForgottenEmailResponse(email);
     }
 
