41a9961278d5d601e674aa5d0d1642f078445a95
e4g3r <qaws5656@naver.com>
2024-09-14 19:55:19 +0900

    feat: 비밀번호 초기화 메일 요청 API

--- src/main/java/com/eager/questioncloud/help/HelpAccountController.java
+++ src/main/java/com/eager/questioncloud/help/HelpAccountController.java
@@ -1,11 +1,13 @@
 package com.eager.questioncloud.help;
 
+import com.eager.questioncloud.common.DefaultResponse;
 import com.eager.questioncloud.help.Request.RecoverForgottenEmailRequest;
 import com.eager.questioncloud.help.Response.RecoverForgottenEmailResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +20,11 @@ public class HelpAccountController {
     public RecoverForgottenEmailResponse recoverForgottenEmail(@RequestBody RecoverForgottenEmailRequest request) {
         String email = helpAccountService.recoverForgottenEmail(request.getPhone());
         return new RecoverForgottenEmailResponse(email);
+    }
+
+    @GetMapping("/recover/forgotten-password/send-mail")
+    public DefaultResponse sendRecoverForgottenPasswordMail(@RequestParam String email) {
+        helpAccountService.sendRecoverForgottenPasswordMail(email);
+        return DefaultResponse.success();
     }
 }
