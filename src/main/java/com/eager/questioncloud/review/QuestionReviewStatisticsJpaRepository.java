2ce70753342e50636b5c8b11dd5bfb53a104979c
e4g3r <qaws5656@naver.com>
2024-09-25 17:39:38 +0900

    feat: QuestionReviewStatistics 관련 레포지토리 추가

---
+++ src/main/java/com/eager/questioncloud/review/QuestionReviewStatisticsJpaRepository.java
@@ -0,0 +1,6 @@
+package com.eager.questioncloud.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionReviewStatisticsJpaRepository extends JpaRepository<QuestionReviewStatisticsEntity, Long> {
+}
