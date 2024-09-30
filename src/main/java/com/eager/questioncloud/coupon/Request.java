be16595ed7491f211fc9d344ab6ba0ffe772349d
e4g3r <qaws5656@naver.com>
2024-09-30 23:42:18 +0900

    feat: RegisterCouponRequest Validate

--- src/main/java/com/eager/questioncloud/coupon/Request.java
+++ src/main/java/com/eager/questioncloud/coupon/Request.java
@@ -1,10 +1,12 @@
 package com.eager.questioncloud.coupon;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 public class Request {
     @Getter
     public static class RegisterCouponRequest {
+        @NotBlank
         private String code;
     }
 }
