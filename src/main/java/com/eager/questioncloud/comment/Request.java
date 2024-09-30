6d075ad2701d3629fc9fe0609ce30e7423a421e0
e4g3r <qaws5656@naver.com>
2024-09-30 23:41:10 +0900

    feat: AddQuestionBoardCommentRequest Validate

--- src/main/java/com/eager/questioncloud/comment/Request.java
+++ src/main/java/com/eager/questioncloud/comment/Request.java
@@ -1,11 +1,16 @@
 package com.eager.questioncloud.comment;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class Request {
     @Getter
     public static class AddQuestionBoardCommentRequest {
+        @NotNull
         private Long boardId;
+
+        @NotBlank
         private String comment;
     }
 
