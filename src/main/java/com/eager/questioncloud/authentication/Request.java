ece95f7a158ecbab3495dc5fbc4b150e9be3ba66
e4g3r <qaws5656@naver.com>
2024-09-30 23:19:10 +0900

    feat: LoginRequest Validate

--- src/main/java/com/eager/questioncloud/authentication/Request.java
+++ src/main/java/com/eager/questioncloud/authentication/Request.java
@@ -1,26 +1,19 @@
 package com.eager.questioncloud.authentication;
 
-import com.eager.questioncloud.exception.CustomException;
-import com.eager.questioncloud.exception.Error;
 import com.eager.questioncloud.valid.EmailValidator;
-import java.util.regex.Pattern;
+import com.eager.questioncloud.valid.PasswordValidator;
+import com.eager.questioncloud.valid.Validatable;
 import lombok.Getter;
 
 public class Request {
     @Getter
-    public static class LoginRequest {
+    public static class LoginRequest implements Validatable {
         private String email;
         private String password;
 
         public void validate() {
             EmailValidator.validate(email);
-            passwordValidate();
-        }
-
-        public void passwordValidate() {
-            if (!Pattern.matches("^(?!.*\\s).{8,}$", password)) {
-                throw new CustomException(Error.BAD_REQUEST);
-            }
+            PasswordValidator.validate(password);
         }
     }
 }
