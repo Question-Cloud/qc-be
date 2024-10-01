719b80b6018264ece2784073bf6d191cb64fb182
e4g3r <qaws5656@naver.com>
2024-10-01 13:42:28 +0900

    feat: ChargePointRequest Enum Valid 추가

--- src/main/java/com/eager/questioncloud/point/Request.java
+++ src/main/java/com/eager/questioncloud/point/Request.java
@@ -1,11 +1,13 @@
 package com.eager.questioncloud.point;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class Request {
     @Getter
     public static class ChargePointRequest {
+        @NotNull
         private ChargePointType chargePointType;
 
         @NotBlank
