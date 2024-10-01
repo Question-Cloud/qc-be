0aadb07a856bc1ef9e0572749aeb35198fbc69b9
e4g3r <qaws5656@naver.com>
2024-10-01 14:24:54 +0900

    feat: redis properties

--- src/main/java/com/eager/questioncloud/config/RedisConfig.java
+++ src/main/java/com/eager/questioncloud/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.eager.questioncloud.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -8,9 +9,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${REDIS_HOST}")
+    private String REDIS_HOST;
+
+    @Value("${REDIS_PORT}")
+    private int REDIS_PORT;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        return new LettuceConnectionFactory(REDIS_HOST, REDIS_PORT);
     }
 
     @Bean
