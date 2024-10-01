4ea8adcd5a5901278eeef7487de6118bb9f3dd9d
e4g3r <qaws5656@naver.com>
2024-10-01 13:46:10 +0900

    feat: QuestionReview Request Validate

--- src/main/java/com/eager/questioncloud/review/Request.java
+++ src/main/java/com/eager/questioncloud/review/Request.java
@@ -1,18 +1,32 @@
 package com.eager.questioncloud.review;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class Request {
     @Getter
     public static class RegisterQuestionReviewRequest {
+        @NotNull
         private Long questionId;
+
+        @Min(value = 1)
+        @Max(value = 5)
         private int rate;
+
+        @NotBlank
         private String comment;
     }
 
     @Getter
     public static class ModifyQuestionReviewRequest {
+        @Min(value = 1)
+        @Max(value = 5)
         private int rate;
+
+        @NotBlank
         private String comment;
     }
 }
