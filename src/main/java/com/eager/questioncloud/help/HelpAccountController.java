152531df8fed2f554711e44534b5e79e1e7b3e6c
e4g3r <qaws5656@naver.com>
2024-10-02 18:21:30 +0900

    feat: HelpAccountController API DOC

--- src/main/java/com/eager/questioncloud/help/HelpAccountController.java
+++ src/main/java/com/eager/questioncloud/help/HelpAccountController.java
@@ -2,6 +2,8 @@ package com.eager.questioncloud.help;
 
 import com.eager.questioncloud.common.DefaultResponse;
 import com.eager.questioncloud.help.Response.RecoverForgottenEmailResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,20 @@ public class HelpAccountController {
     private final HelpAccountService helpAccountService;
 
     @GetMapping("/recover/email")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(operationId = "계정 찾기", summary = "계정 찾기", tags = {"help"}, description = "계정 찾기")
     public RecoverForgottenEmailResponse recoverForgottenEmail(@RequestParam String phone) {
         String email = helpAccountService.recoverForgottenEmail(phone);
         return new RecoverForgottenEmailResponse(email);
     }
 
     @GetMapping("/recover/password")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(operationId = "비밀번호 찾기", summary = "비밀번호 찾기", tags = {"help"}, description = "비밀번호 찾기")
     public DefaultResponse sendRecoverForgottenPasswordMail(@RequestParam String email) {
         helpAccountService.sendRecoverForgottenPasswordMail(email);
         return DefaultResponse.success();
