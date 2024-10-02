8c726c2d5f81240b5cd6b24cce33b066e15dca34
e4g3r <qaws5656@naver.com>
2024-10-02 21:10:40 +0900

    feat: SwaggerConfig에서 문서에 필요없는 파라미터 일괄 제거 처리

--- src/main/java/com/eager/questioncloud/config/SwaggerConfig.java
+++ src/main/java/com/eager/questioncloud/config/SwaggerConfig.java
@@ -3,6 +3,9 @@ package com.eager.questioncloud.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import java.util.List;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +21,18 @@ public class SwaggerConfig {
         return new OpenAPI()
             .components(new Components())
             .info(info);
+    }
+
+    @Bean
+    public OpenApiCustomizer removePageable() {
+        return openApi -> openApi.getPaths().values().forEach(pathItem -> {
+            pathItem.readOperationsMap().values().forEach(operation -> {
+                List<Parameter> parameters = operation.getParameters();
+                if (parameters != null) {
+                    parameters.removeIf(param -> "pageable".equals(param.getName()));
+                    parameters.removeIf(param -> "userId".equals(param.getName()));
+                }
+            });
+        });
     }
 }
