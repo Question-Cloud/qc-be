04360f7fefc0a05012d1ac29b578b8b1a9242115
e4g3r <qaws5656@naver.com>
2024-10-02 18:22:40 +0900

    feat: UserQuestionLibraryController API DOC

--- src/main/java/com/eager/questioncloud/library/UserQuestionLibraryController.java
+++ src/main/java/com/eager/questioncloud/library/UserQuestionLibraryController.java
@@ -3,6 +3,8 @@ package com.eager.questioncloud.library;
 import com.eager.questioncloud.common.PagingResponse;
 import com.eager.questioncloud.library.UserQuestionLibraryDto.UserQuestionLibraryItem;
 import com.eager.questioncloud.question.QuestionFilter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,10 @@ public class UserQuestionLibraryController {
     private final UserQuestionLibraryService userQuestionLibraryService;
 
     @GetMapping
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(operationId = "나의 문제 목록 조회", summary = "나의 문제 목록 조회", tags = {"library"}, description = "나의 문제 목록 조회")
     public PagingResponse<UserQuestionLibraryItem> getUserQuestionLibraryList(QuestionFilter questionFilter) {
         int total = userQuestionLibraryService.countUserQuestions(questionFilter);
         List<UserQuestionLibraryItem> userQuestions = userQuestionLibraryService.getUserQuestions(questionFilter);
