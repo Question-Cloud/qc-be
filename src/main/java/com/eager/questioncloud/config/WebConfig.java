3d3879d4ab6bf7f2028e29dcbe985ea8d75a18cb
e4g3r <qaws5656@naver.com>
2024-10-10 13:33:24 +0900

    refactor: MvcConfig 로직 WebConfig로 통합

--- src/main/java/com/eager/questioncloud/config/WebConfig.java
+++ src/main/java/com/eager/questioncloud/config/WebConfig.java
@@ -4,7 +4,9 @@ import com.eager.questioncloud.resolver.QuestionFilterArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +17,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(questionFilterArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.OPTIONS.name())
+            .allowedOrigins("*");
     }
 }
