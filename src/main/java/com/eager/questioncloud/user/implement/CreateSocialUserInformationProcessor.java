6fd20cfe9217e107083e5b5a30196437f7ab591c
e4g3r <qaws5656@naver.com>
2024-10-11 21:49:15 +0900

    refactor: CreateSocialUserInformationProcessor 내부 create 함수 제거  - 필요한 곳에서 CreateSocialUserInformationAppender를 직접 사용

--- src/main/java/com/eager/questioncloud/user/implement/CreateSocialUserInformationProcessor.java
+++ src/main/java/com/eager/questioncloud/user/implement/CreateSocialUserInformationProcessor.java
@@ -8,13 +8,8 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class CreateSocialUserInformationProcessor {
-    private final CreateSocialUserInformationAppender createSocialUserInformationAppender;
     private final CreateSocialUserInformationReader createSocialUserInformationReader;
     private final CreateSocialUserInformationUpdater createSocialUserInformationUpdater;
-
-    public CreateSocialUserInformation create(CreateSocialUserInformation createSocialUserInformation) {
-        return createSocialUserInformationAppender.append(createSocialUserInformation);
-    }
 
     public CreateSocialUserInformation use(String token, AccountType accountType) {
         CreateSocialUserInformation createSocialUserInformation = createSocialUserInformationReader.find(token, accountType);
