67608462211652bd0fa64ed96cf38743ba14e177
e4g3r <qaws5656@naver.com>
2024-10-28 21:43:48 +0900

    feat: qc-core PortOne API 추가

---
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/portone/dto/PortonePayment.java
@@ -0,0 +1,19 @@
+package com.eager.questioncloud.core.domain.portone.dto;
+
+import com.eager.questioncloud.core.domain.portone.enums.PortonePaymentStatus;
+import lombok.Getter;
+
+@Getter
+public class PortonePayment {
+    private String id;
+    private PortonePaymentStatus status;
+    private PortonePaymentAmount amount;
+    private String receiptUrl;
+
+    @Getter
+    public static class PortonePaymentAmount {
+        private int total;
+        private int taxFree;
+        private int vat;
+    }
+}
---
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/portone/implement/PortoneAPI.java
@@ -0,0 +1,50 @@
+package com.eager.questioncloud.core.domain.portone.implement;
+
+import com.eager.questioncloud.core.domain.portone.dto.PortoneCancelRequest;
+import com.eager.questioncloud.core.domain.portone.dto.PortonePayment;
+import com.eager.questioncloud.exception.CustomException;
+import com.eager.questioncloud.exception.Error;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@Component
+@RequiredArgsConstructor
+public class PortoneAPI {
+    @Value("${PORT_ONE_SECRET_KEY}")
+    private String PORT_ONE_SECRET_KEY;
+
+    public PortonePayment getPaymentResult(String paymentId) {
+        WebClient webClient = WebClient.create("https://api.portone.io");
+
+        PortonePayment portonePayment = webClient.get()
+            .uri("/payments/%s".formatted(paymentId))
+            .headers(headers -> headers.set("Authorization", "Portone %s".formatted(PORT_ONE_SECRET_KEY)))
+            .exchangeToMono(response -> {
+                if (response.statusCode().is2xxSuccessful()) {
+                    return response.bodyToMono(PortonePayment.class);
+                }
+                throw new CustomException(Error.PAYMENT_ERROR);
+            })
+            .block();
+
+        if (portonePayment == null || portonePayment.getId() == null) {
+            throw new CustomException(Error.PAYMENT_ERROR);
+        }
+
+        return portonePayment;
+    }
+
+    public void cancel(String paymentId) {
+        WebClient webClient = WebClient.create("https://api.portone.io");
+        webClient.post()
+            .uri("/payments/%s/cancel".formatted(paymentId))
+            .headers(headers -> headers.set("Authorization", "Portone %s".formatted(PORT_ONE_SECRET_KEY)))
+            .body(BodyInserters.fromValue(new PortoneCancelRequest("Invalid Payment")))
+            .exchangeToMono(response -> response.bodyToMono(Void.class))
+            .subscribe();
+    }
+}
