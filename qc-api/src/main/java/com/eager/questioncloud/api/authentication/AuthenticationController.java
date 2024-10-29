d4f4be679691372fc86b1a55aa7011e6b9147b61
e4g3r <qaws5656@naver.com>
2024-10-29 20:40:09 +0900

    feat: Swagger 문서 추가

--- qc-api/src/main/java/com/eager/questioncloud/api/authentication/AuthenticationController.java
+++ qc-api/src/main/java/com/eager/questioncloud/api/authentication/AuthenticationController.java
@@ -8,6 +8,8 @@ import com.eager.questioncloud.core.domain.authentication.service.Authentication
 import com.eager.questioncloud.core.domain.authentication.vo.AuthenticationToken;
 import com.eager.questioncloud.core.domain.social.dto.SocialAuthenticateResult;
 import com.eager.questioncloud.core.domain.user.vo.AccountType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,18 +25,37 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(operationId = "로그인", summary = "로그인", tags = {"auth"}, description = "로그인")
     public LoginResponse login(@RequestBody LoginRequest loginRequest) {
         AuthenticationToken authenticationToken = authenticationService.login(loginRequest.getEmail(), loginRequest.getPassword());
         return new LoginResponse(authenticationToken);
     }
 
     @PostMapping("/refresh")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(operationId = "토큰 리프레시", summary = "토큰 리프레시", tags = {"auth"}, description = "토큰 리프레시")
     public RefreshResponse refresh(@RequestParam String refreshToken) {
         AuthenticationToken authenticationToken = authenticationService.refresh(refreshToken);
         return new RefreshResponse(authenticationToken);
     }
 
     @GetMapping("/social")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(
+        operationId = "소셜 로그인", summary = "소셜 로그인", tags = {"auth"},
+        description = """
+                이미 등록되어 있는 소셜 계정이라면 isRegistered: True, authenticationToken이 응답으로 주어집니다.
+                등록되어 있지 않은 소셜 계졍이라면 isRegistered: False, registerToken이 응답으로 주어집니다.
+                registerToken은 회원가입 시 사용됩니다.
+            """
+    )
     public SocialAuthenticateResponse socialLogin(@RequestParam AccountType accountType, @RequestParam String code) {
         SocialAuthenticateResult socialAuthenticateResult = authenticationService.socialLogin(accountType, code);
         return SocialAuthenticateResponse.create(socialAuthenticateResult);
--- qc-api/src/main/java/com/eager/questioncloud/api/user/CreateUserController.java
+++ qc-api/src/main/java/com/eager/questioncloud/api/user/CreateUserController.java
@@ -7,6 +7,8 @@ import com.eager.questioncloud.core.domain.user.model.User;
 import com.eager.questioncloud.core.domain.user.service.CreateUserService;
 import com.eager.questioncloud.core.domain.verification.model.EmailVerification;
 import com.eager.questioncloud.core.domain.verification.vo.EmailVerificationType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,10 @@ public class CreateUserController {
     private final CreateUserService createUserService;
 
     @PostMapping
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(operationId = "회원가입", summary = "회원가입", tags = {"user-register"}, description = "회원가입")
     public CreateUserResponse createUser(@RequestBody @Valid Request.CreateUserRequest request) {
         User user = createUserService.create(
             new CreateUser(request.getEmail(), request.getPassword(), request.getSocialRegisterToken(), request.getAccountType(), request.getPhone(),
@@ -33,12 +39,20 @@ public class CreateUserController {
     }
 
     @GetMapping("/resend-verification-mail")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(operationId = "회원가입 인증 메일 재요청", summary = "회원가입 인증 메일 재요청", tags = {"user-register"}, description = "회원가입 인증 메일 재요청")
     public DefaultResponse resendVerificationMail(@RequestParam String resendToken) {
         createUserService.resend(resendToken);
         return DefaultResponse.success();
     }
 
     @GetMapping("/verify")
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
+    })
+    @Operation(operationId = "회원가입 인증 메일 확인", summary = "회원가입 인증 메일 확인", tags = {"user-register"}, description = "회원가입 인증 메일 확인")
     public DefaultResponse verifyCreateUser(@RequestParam String token) {
         createUserService.verifyCreateUser(token, EmailVerificationType.CreateUser);
         return DefaultResponse.success();
