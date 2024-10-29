12a4660a1d8829d7a0d5b62a13dd721225b2cf9c
e4g3r <qaws5656@naver.com>
2024-10-29 16:56:33 +0900

    feat: qc-core,qc-storage,qc-api Subscribe Domain 추가

---
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/subscribe/implement/SubscribeProcessor.java
@@ -0,0 +1,33 @@
+package com.eager.questioncloud.core.domain.subscribe.implement;
+
+import com.eager.questioncloud.core.domain.creator.implement.CreatorReader;
+import com.eager.questioncloud.core.domain.subscribe.model.Subscribe;
+import com.eager.questioncloud.exception.CustomException;
+import com.eager.questioncloud.exception.Error;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SubscribeProcessor {
+    private final SubscribeReader subscribeReader;
+    private final SubscribeAppender subscribeAppender;
+    private final CreatorReader creatorReader;
+    private final SubscribeRemover subscribeRemover;
+
+    public Subscribe subscribe(Long userId, Long creatorId) {
+        if (!creatorReader.isExistsCreator(creatorId)) {
+            throw new CustomException(Error.NOT_FOUND);
+        }
+
+        if (subscribeReader.isSubscribed(userId, creatorId)) {
+            throw new CustomException(Error.ALREADY_SUBSCRIBE_CREATOR);
+        }
+
+        return subscribeAppender.append(Subscribe.create(userId, creatorId));
+    }
+
+    public void unSubscribe(Long userId, Long creatorId) {
+        subscribeRemover.unSubscribe(userId, creatorId);
+    }
+}
---
+++ qc-storage/src/main/java/com/eager/questioncloud/storage/subscribe/SubscribeRepositoryImpl.java
@@ -0,0 +1,82 @@
+package com.eager.questioncloud.storage.subscribe;
+
+import static com.eager.questioncloud.storage.creator.QCreatorEntity.creatorEntity;
+import static com.eager.questioncloud.storage.subscribe.QSubscribeEntity.subscribeEntity;
+import static com.eager.questioncloud.storage.user.QUserEntity.userEntity;
+
+import com.eager.questioncloud.core.common.PagingInformation;
+import com.eager.questioncloud.core.domain.creator.dto.CreatorDto.CreatorSimpleInformation;
+import com.eager.questioncloud.core.domain.subscribe.dto.SubscribeDto.SubscribeListItem;
+import com.eager.questioncloud.core.domain.subscribe.model.Subscribe;
+import com.eager.questioncloud.core.domain.subscribe.repository.SubscribeRepository;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class SubscribeRepositoryImpl implements SubscribeRepository {
+    private final SubscribeJpaRepository subscribeJpaRepository;
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Subscribe save(Subscribe subscribe) {
+        return subscribeJpaRepository.save(SubscribeEntity.from(subscribe)).toModel();
+    }
+
+    @Override
+    public Boolean isSubscribed(Long subscriberId, Long creatorId) {
+        return subscribeJpaRepository.existsBySubscriberIdAndCreatorId(subscriberId, creatorId);
+    }
+
+    @Override
+    public void unSubscribe(Long subscriberId, Long creatorId) {
+        subscribeJpaRepository.deleteBySubscriberIdAndCreatorId(subscriberId, creatorId);
+    }
+
+    @Override
+    public int countSubscriber(Long creatorId) {
+        return subscribeJpaRepository.countByCreatorId(creatorId);
+    }
+
+    @Override
+    public List<SubscribeListItem> getMySubscribeList(Long userId, PagingInformation pagingInformation) {
+        QSubscribeEntity countCheck = new QSubscribeEntity("count");
+        return jpaQueryFactory.select(
+                Projections.constructor(
+                    SubscribeListItem.class,
+                    subscribeEntity.id,
+                    Projections.constructor(CreatorSimpleInformation.class,
+                        creatorEntity.id,
+                        userEntity.userInformationEntity.name,
+                        userEntity.userInformationEntity.profileImage,
+                        creatorEntity.creatorProfileEntity.mainSubject,
+                        creatorEntity.creatorProfileEntity.introduction,
+                        countCheck.id.count().intValue())))
+            .from(subscribeEntity)
+            .where(subscribeEntity.subscriberId.eq(userId))
+            .leftJoin(creatorEntity).on(creatorEntity.id.eq(subscribeEntity.creatorId))
+            .leftJoin(userEntity).on(userEntity.uid.eq(creatorEntity.userId))
+            .leftJoin(countCheck).on(countCheck.creatorId.eq(creatorEntity.id))
+            .groupBy(subscribeEntity.id)
+            .offset(pagingInformation.getPage())
+            .limit(pagingInformation.getSize())
+            .fetch();
+    }
+
+    @Override
+    public int countMySubscribe(Long userId) {
+        Integer count = jpaQueryFactory.select(subscribeEntity.id.count().intValue())
+            .from(subscribeEntity)
+            .where(subscribeEntity.subscriberId.eq(userId))
+            .fetchFirst();
+
+        if (count == null) {
+            return 0;
+        }
+
+        return count;
+    }
+}
