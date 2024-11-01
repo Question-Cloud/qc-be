0a4cf72ee8ebfc60dc3b5777a5decd9d1eda5f50
e4g3r <qaws5656@naver.com>
2024-11-02 01:50:27 +0900

    refactor: UserPoint 구현체 이름 변경 UserPointProcessor -> UserPointManager

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/point/service/ChargePointService.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/point/service/ChargePointService.java
@@ -1,7 +1,7 @@
 package com.eager.questioncloud.core.domain.payment.point.service;
 
 import com.eager.questioncloud.core.domain.payment.point.implement.ChargePointPaymentProcessor;
-import com.eager.questioncloud.core.domain.payment.point.implement.UserPointProcessor;
+import com.eager.questioncloud.core.domain.payment.point.implement.UserPointManager;
 import com.eager.questioncloud.core.domain.payment.point.model.ChargePointPayment;
 import com.eager.questioncloud.core.domain.portone.dto.PortonePayment;
 import com.eager.questioncloud.core.domain.portone.implement.PortoneAPI;
@@ -12,17 +12,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ChargePointService {
     private final ChargePointPaymentProcessor chargePointPaymentProcessor;
-    private final UserPointProcessor userPointProcessor;
+    private final UserPointManager userPointManager;
     private final PortoneAPI portoneAPI;
 
     public void createOrder(ChargePointPayment chargePointPayment) {
         chargePointPaymentProcessor.createOrder(chargePointPayment);
     }
-    
+
     public void paymentAndCharge(String paymentId) {
         PortonePayment portonePayment = portoneAPI.getPaymentResult(paymentId);
         ChargePointPayment chargePointPayment = chargePointPaymentProcessor.payment(portonePayment);
-        userPointProcessor.chargePoint(
+        userPointManager.chargePoint(
             chargePointPayment.getUserId(),
             chargePointPayment.getChargePointType()
         );
