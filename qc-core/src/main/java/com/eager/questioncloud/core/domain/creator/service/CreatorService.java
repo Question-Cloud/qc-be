0e263742ca1d024160857ed21fe0673d79ab81f6
e4g3r <qaws5656@naver.com>
2024-11-02 21:20:41 +0900

    refactor: 크리에이터 추가하는 구현체 CreatorRegister로 변경  - CreatorAppender는 단독으로 사용될 일이 없으므로 CreatorRegister 구현체에서 크리에이터 추가 구현을 담당하도록 함

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/creator/service/CreatorService.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/creator/service/CreatorService.java
@@ -1,12 +1,11 @@
 package com.eager.questioncloud.core.domain.creator.service;
 
 import com.eager.questioncloud.core.domain.creator.dto.CreatorDto.CreatorInformation;
-import com.eager.questioncloud.core.domain.creator.implement.CreatorAppender;
 import com.eager.questioncloud.core.domain.creator.implement.CreatorReader;
+import com.eager.questioncloud.core.domain.creator.implement.CreatorRegister;
 import com.eager.questioncloud.core.domain.creator.implement.CreatorUpdater;
 import com.eager.questioncloud.core.domain.creator.model.Creator;
 import com.eager.questioncloud.core.domain.creator.vo.CreatorProfile;
-import com.eager.questioncloud.core.domain.user.implement.UserUpdater;
 import com.eager.questioncloud.core.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,14 +13,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CreatorService {
-    private final CreatorAppender creatorAppender;
+    private final CreatorRegister creatorRegister;
     private final CreatorReader creatorReader;
     private final CreatorUpdater creatorUpdater;
-    private final UserUpdater userUpdater;
 
     public Creator register(User user, CreatorProfile creatorProfile) {
-        userUpdater.setCreator(user);
-        return creatorAppender.append(Creator.create(user.getUid(), creatorProfile));
+        return creatorRegister.register(user, creatorProfile);
     }
 
     public CreatorInformation getCreatorInformation(Long creatorId) {
