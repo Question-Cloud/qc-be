a9ac410828ab320c02f10996846059a2fd6fa278
e4g3r <qaws5656@naver.com>
2024-11-05 14:40:31 +0900

    refactor: ChargePointEventListener.chargePointEvent 예외 처리 로직 추가

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/point/event/ChargePointEventListener.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/point/event/ChargePointEventListener.java
@@ -1,7 +1,10 @@
 package com.eager.questioncloud.core.domain.payment.point.event;
 
+import com.eager.questioncloud.core.domain.payment.point.implement.ChargePointPaymentExceptionHandler;
 import com.eager.questioncloud.core.domain.payment.point.implement.UserPointManager;
 import com.eager.questioncloud.core.domain.payment.point.model.ChargePointEvent;
+import com.eager.questioncloud.core.exception.CustomException;
+import com.eager.questioncloud.core.exception.Error;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -10,12 +13,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ChargePointEventListener {
     private final UserPointManager userPointManager;
+    private final ChargePointPaymentExceptionHandler chargePointPaymentExceptionHandler;
 
     @EventListener
     public void chargePointEvent(ChargePointEvent chargePointEvent) {
-        userPointManager.chargePoint(
-            chargePointEvent.getUserId(),
-            chargePointEvent.getChargePointType()
-        );
+        try {
+            userPointManager.chargePoint(chargePointEvent.getUserId(), chargePointEvent.getChargePointType());
+        } catch (Exception e) {
+            chargePointPaymentExceptionHandler.failHandler(chargePointEvent.getPaymentId());
+            throw new CustomException(Error.PAYMENT_ERROR);
+        }
     }
 }
