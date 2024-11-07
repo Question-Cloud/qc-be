5ce27e2477821e4d745dd31bdbd88bf0fcb4dd31
e4g3r <qaws5656@naver.com>
2024-11-07 14:02:44 +0900

    feat: 문제 결제 시 보유하고 있는 문제인지 검사하는 로직 추가

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/question/service/QuestionPaymentService.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/question/service/QuestionPaymentService.java
@@ -1,10 +1,13 @@
 package com.eager.questioncloud.core.domain.payment.question.service;
 
 import com.eager.questioncloud.core.domain.feed.library.implement.UserQuestionAppender;
+import com.eager.questioncloud.core.domain.feed.library.implement.UserQuestionReader;
 import com.eager.questioncloud.core.domain.hub.question.implement.QuestionReader;
 import com.eager.questioncloud.core.domain.hub.question.model.Question;
 import com.eager.questioncloud.core.domain.payment.question.implement.QuestionPaymentProcessor;
 import com.eager.questioncloud.core.domain.payment.question.model.QuestionPayment;
+import com.eager.questioncloud.core.exception.CustomException;
+import com.eager.questioncloud.core.exception.Error;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,10 +18,18 @@ public class QuestionPaymentService {
     private final QuestionPaymentProcessor paymentProcessor;
     private final UserQuestionAppender userQuestionAppender;
     private final QuestionReader questionReader;
+    private final UserQuestionReader userQuestionReader;
 
     public void payment(Long userId, List<Long> questionIds, Long userCouponId) {
+        checkAlreadyOwn(userId, questionIds);
         List<Question> questions = questionReader.getQuestions(questionIds);
         paymentProcessor.questionPayment(QuestionPayment.create(userId, userCouponId, questions));
         userQuestionAppender.appendUserQuestions(userId, questionIds);
+    }
+
+    private void checkAlreadyOwn(Long userId, List<Long> questionIds) {
+        if (userQuestionReader.isOwned(userId, questionIds)) {
+            throw new CustomException(Error.ALREADY_OWN_QUESTION);
+        }
     }
 }
