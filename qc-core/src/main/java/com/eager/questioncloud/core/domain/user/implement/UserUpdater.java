c8ea16972d831c587e9c927c4a7b031d1fb12949
e4g3r <qaws5656@naver.com>
2024-11-08 12:40:56 +0900

    refactor: UserUpdater 메서드 리팩토링  - 반환값 필요없다고 판단하여 void 처리

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/user/implement/UserUpdater.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/user/implement/UserUpdater.java
@@ -10,18 +10,18 @@ import org.springframework.stereotype.Component;
 public class UserUpdater {
     private final UserRepository userRepository;
 
-    public User verifyUser(User user) {
+    public void verifyUser(User user) {
         user.active();
-        return userRepository.save(user);
+        userRepository.save(user);
     }
 
-    public User updateUser(User user, String name, String profileImage) {
-        return userRepository.save(user.update(name, profileImage));
+    public void updateUser(User user, String name, String profileImage) {
+        userRepository.save(user.update(name, profileImage));
     }
 
-    public User changePassword(User user, String newPassword) {
+    public void changePassword(User user, String newPassword) {
         user.changePassword(newPassword);
-        return userRepository.save(user);
+        userRepository.save(user);
     }
 
     public void setCreator(User user) {
