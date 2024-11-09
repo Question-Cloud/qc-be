6c9ed9da9b11ff572dab846ee102cfb161af030e
e4g3r <qaws5656@naver.com>
2024-11-09 16:12:21 +0900

    feat: UpdateReviewStatisticsEvent 처리 로직 추가  - UpdateReviewStatisticsEvent 핸들러 추가  - QuestionReviewStatistics.update 메서드 추가 -> 이벤트 타입에 따라 통계 관련 데이터 업데이트 처리 로직

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/hub/review/model/QuestionReviewStatistics.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/hub/review/model/QuestionReviewStatistics.java
@@ -1,5 +1,6 @@
 package com.eager.questioncloud.core.domain.hub.review.model;
 
+import com.eager.questioncloud.core.domain.hub.review.event.UpdateReviewStatisticsEvent;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -25,6 +26,14 @@ public class QuestionReviewStatistics {
             .totalRate(0)
             .averageRate(0.0)
             .build();
+    }
+
+    public void update(UpdateReviewStatisticsEvent updateReviewStatisticsEvent) {
+        switch (updateReviewStatisticsEvent.getUpdateReviewType()) {
+            case REGISTER -> updateByNewReview(updateReviewStatisticsEvent.getVarianceRate());
+            case MODIFY -> updateByModifyReview(updateReviewStatisticsEvent.getVarianceRate());
+            case DELETE -> updateByDeleteReview(updateReviewStatisticsEvent.getVarianceRate());
+        }
     }
 
     public void updateByNewReview(int newRate) {
