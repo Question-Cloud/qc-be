dc346c06e773040c33cff49f19a4e7c6d6e71993
e4g3r <qaws5656@naver.com>
2024-11-11 19:45:07 +0900

    fix: QuestionController endPoint 수정 - url에 hub 추가

--- qc-api/src/main/java/com/eager/questioncloud/api/hub/question/QuestionCategoryController.java
+++ qc-api/src/main/java/com/eager/questioncloud/api/hub/question/QuestionCategoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/question/category")
+@RequestMapping("/api/hub/question/category")
 @RequiredArgsConstructor
 public class QuestionCategoryController {
     private final QuestionCategoryService questionCategoryService;
