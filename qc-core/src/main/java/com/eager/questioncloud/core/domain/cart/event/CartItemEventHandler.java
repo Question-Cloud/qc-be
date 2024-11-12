b0a99cd694cdfc03df5103e1bca22a9e16fcf67f
e4g3r <qaws5656@naver.com>
2024-11-12 22:27:08 +0900

    fix: clearCartItem 트랜젝션 처리

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/cart/event/CartItemEventHandler.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/cart/event/CartItemEventHandler.java
@@ -4,6 +4,7 @@ import com.eager.questioncloud.core.domain.cart.repository.CartItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -11,6 +12,7 @@ public class CartItemEventHandler {
     private final CartItemRepository cartItemRepository;
 
     @EventListener
+    @Transactional
     public void clearCartItem(ClearCartItemEvent clearCartItemEvent) {
         cartItemRepository.deleteByQuestionIdInAndUserId(clearCartItemEvent.getQuestionIds(), clearCartItemEvent.getUserId());
     }
