d15a1c40a66637042a8f624548c1aa7f0ecc0d20
e4g3r <qaws5656@naver.com>
2024-11-12 16:36:18 +0900

    feat: 장바구니 도메인 추가  - 유저는 하나의 장바구니를 소유 할 수 있음  - 장바구니 안에는 문제 이름, 크리에이터 이름, 과목, 가격, 썸네일 정보가 있음

---
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/cart/model/CartItem.java
@@ -0,0 +1,28 @@
+package com.eager.questioncloud.core.domain.cart.model;
+
+import com.eager.questioncloud.core.domain.cart.vo.CartItemInformation;
+import com.eager.questioncloud.core.domain.question.model.Question;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CartItem {
+    private Long id;
+    private Long userId;
+    private CartItemInformation itemInformation;
+
+    @Builder
+    private CartItem(Long id, Long userId, CartItemInformation itemInformation) {
+        this.id = id;
+        this.userId = userId;
+        this.itemInformation = itemInformation;
+    }
+
+    public static CartItem create(Long userId, Question question) {
+        return CartItem.builder()
+            .userId(userId)
+            .itemInformation(CartItemInformation.create(question))
+            .build();
+    }
+}
+
