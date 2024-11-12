a5d61dee1864b66351e97ed5904d95472d55cbb8
e4g3r <qaws5656@naver.com>
2024-11-12 20:17:14 +0900

    feat: CartItemRemover.remove 트랜젝션 처리

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/cart/implement/CartItemRemover.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/cart/implement/CartItemRemover.java
@@ -3,12 +3,14 @@ package com.eager.questioncloud.core.domain.cart.implement;
 import com.eager.questioncloud.core.domain.cart.repository.CartItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class CartItemRemover {
     private final CartItemRepository cartItemRepository;
 
+    @Transactional
     public void remove(Long userId, Long cartItemId) {
         cartItemRepository.deleteByIdAndUserId(cartItemId, userId);
     }
