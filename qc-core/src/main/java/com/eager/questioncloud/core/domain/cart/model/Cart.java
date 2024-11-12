05df37fb7ec98e68731181cfbc3e5f6db8ad55a4
e4g3r <qaws5656@naver.com>
2024-11-12 17:22:27 +0900

    feat: Cart 도메인 클래스 추가  - 유저의 장바구니 개념

---
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/cart/model/Cart.java
@@ -0,0 +1,17 @@
+package com.eager.questioncloud.core.domain.cart.model;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Cart {
+    private Long userId;
+    private List<CartItem> items;
+
+    @Builder
+    private Cart(Long userId, List<CartItem> items) {
+        this.userId = userId;
+        this.items = items;
+    }
+}
