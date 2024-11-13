38c8981399628a20973413ac1b08cd126e4cf1f4
e4g3r <qaws5656@naver.com>
2024-11-13 19:01:03 +0900

    refactor: review 관련 entity, repository 구현체 분리

--- qc-storage/src/main/java/com/eager/questioncloud/storage/review/QuestionReviewStatisticsJpaRepository.java
+++ qc-storage/src/main/java/com/eager/questioncloud/storage/review/QuestionReviewStatisticsJpaRepository.java
@@ -1,4 +1,4 @@
-package com.eager.questioncloud.storage.question;
+package com.eager.questioncloud.storage.review;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
