6a7a924d007715080c9c585132853ca3b0a66eab
e4g3r <qaws5656@naver.com>
2024-11-14 19:43:22 +0900

    Revert "feat: 문제 구매 내역 QuestionPaymentHistory 조회 로직 추가"
    This reverts commit afc8a4d6003e285db717024935851bdf3ac80a02.

--- qc-api/src/main/java/com/eager/questioncloud/api/payment/question/QuestionPaymentController.java
+++ qc-api/src/main/java/com/eager/questioncloud/api/payment/question/QuestionPaymentController.java
@@ -2,17 +2,12 @@ package com.eager.questioncloud.api.payment.question;
 
 import com.eager.questioncloud.api.payment.question.Request.QuestionPaymentRequest;
 import com.eager.questioncloud.common.DefaultResponse;
-import com.eager.questioncloud.common.PagingResponse;
-import com.eager.questioncloud.core.common.PagingInformation;
-import com.eager.questioncloud.core.domain.payment.model.QuestionPaymentHistory;
-import com.eager.questioncloud.core.domain.payment.service.QuestionPaymentHistoryService;
 import com.eager.questioncloud.core.domain.payment.service.QuestionPaymentService;
 import com.eager.questioncloud.security.UserPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class QuestionPaymentController {
     private final QuestionPaymentService questionPaymentService;
-    private final QuestionPaymentHistoryService questionPaymentHistoryService;
 
     @PostMapping
     @ApiResponses(value = {
@@ -33,17 +27,5 @@ public class QuestionPaymentController {
     public DefaultResponse questionPayment(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestBody QuestionPaymentRequest request) {
         questionPaymentService.payment(userPrincipal.getUser().getUid(), request.getQuestionIds(), request.getUserCouponId());
         return DefaultResponse.success();
-    }
-
-    @GetMapping("/history")
-    @ApiResponses(value = {
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
-    })
-    @Operation(operationId = "문제 구매 내역 조회", summary = "문제 구매 내역 조회", tags = {"question-payment"}, description = "문제 구매 내역 조회")
-    public PagingResponse<QuestionPaymentHistory> getQuestionPaymentHistory(
-        @AuthenticationPrincipal UserPrincipal userPrincipal, PagingInformation pagingInformation) {
-        return new PagingResponse<>(
-            questionPaymentHistoryService.countQuestionPaymentHistory(userPrincipal.getUser().getUid()),
-            questionPaymentHistoryService.getQuestionPaymentHistory(userPrincipal.getUser().getUid(), pagingInformation));
     }
 }
