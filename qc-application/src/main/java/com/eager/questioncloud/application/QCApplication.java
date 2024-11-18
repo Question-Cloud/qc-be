ba6bd2b381f20fe14474b433042d92ea029b4fd6
e4g3r <qaws5656@naver.com>
2024-11-18 19:58:03 +0900

    feat: qc-application 모듈 추가  - Spring을 통한 API 제공 및 비즈니스 처리 모듈

---
+++ qc-application/src/main/java/com/eager/questioncloud/application/QCApplication.java
@@ -0,0 +1,12 @@
+package com.eager.questioncloud.application;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QCApplication.class, args);
+    }
+}
