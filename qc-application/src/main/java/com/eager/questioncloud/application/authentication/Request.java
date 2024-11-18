a2b5690472197b9b7f13bc6ef8db24258a9f96a2
e4g3r <qaws5656@naver.com>
2024-11-18 20:19:35 +0900

    feat: qc-application 인증 관련 Controller 추가

---
+++ qc-application/src/main/java/com/eager/questioncloud/application/authentication/Request.java
@@ -0,0 +1,19 @@
+package com.eager.questioncloud.application.authentication;
+
+import com.eager.questioncloud.application.validator.EmailValidator;
+import com.eager.questioncloud.application.validator.PasswordValidator;
+import com.eager.questioncloud.application.validator.Validatable;
+import lombok.Getter;
+
+public class Request {
+    @Getter
+    public static class LoginRequest implements Validatable {
+        private String email;
+        private String password;
+
+        public void validate() {
+            EmailValidator.validate(email);
+            PasswordValidator.validate(password);
+        }
+    }
+}
