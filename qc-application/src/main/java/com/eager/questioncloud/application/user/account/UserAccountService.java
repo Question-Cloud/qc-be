9f302253d62a34b8a844410adf15b634c75d7ff5
e4g3r <qaws5656@naver.com>
2024-11-20 00:34:58 +0900

    TODO 추가

--- qc-application/src/main/java/com/eager/questioncloud/application/user/account/UserAccountService.java
+++ qc-application/src/main/java/com/eager/questioncloud/application/user/account/UserAccountService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class UserAccountService {
+    //TODO 이메일 인증 관련 로직 및 Repository 별도 클래스로 분리
     private final UserRepository userRepository;
     private final EmailVerificationRepository emailVerificationRepository;
     private final EmailSender emailSender;
