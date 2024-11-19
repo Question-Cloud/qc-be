6595e369513e4f8e795d2ae73704b57cee0e5cc3
e4g3r <qaws5656@naver.com>
2024-11-19 18:26:32 +0900

    refactor: QuestionHub 불필요 구현체 레이어 제거

--- qc-application/src/main/java/com/eager/questioncloud/application/hub/QuestionHubService.java
+++ qc-application/src/main/java/com/eager/questioncloud/application/hub/QuestionHubService.java
@@ -1,8 +1,11 @@
 package com.eager.questioncloud.application.hub;
 
 import com.eager.questioncloud.domain.question.QuestionCategoryGroupBySubject;
+import com.eager.questioncloud.domain.question.QuestionCategoryGroupBySubject.MainQuestionCategory;
+import com.eager.questioncloud.domain.question.QuestionCategoryRepository;
 import com.eager.questioncloud.domain.question.QuestionFilter;
 import com.eager.questioncloud.domain.question.QuestionInformation;
+import com.eager.questioncloud.domain.question.QuestionRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,22 +13,23 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class QuestionHubService {
-    private final QuestionHubReader questionHubReader;
-    private final QuestionCategoryReader questionCategoryReader;
+    private final QuestionRepository questionRepository;
+    private final QuestionCategoryRepository questionCategoryRepository;
 
     public int getTotalFiltering(QuestionFilter questionFilter) {
-        return questionHubReader.countByQuestionFilter(questionFilter);
+        return questionRepository.countByQuestionFilter(questionFilter);
     }
 
     public List<QuestionInformation> getQuestionListByFiltering(QuestionFilter questionFilter) {
-        return questionHubReader.getQuestions(questionFilter);
+        return questionRepository.getQuestionInformation(questionFilter);
     }
 
     public QuestionInformation getQuestionInformation(Long questionId, Long userId) {
-        return questionHubReader.getQuestion(questionId, userId);
+        return questionRepository.getQuestionInformation(questionId, userId);
     }
 
     public List<QuestionCategoryGroupBySubject> getQuestionCategories() {
-        return questionCategoryReader.getQuestionCategories();
+        List<MainQuestionCategory> mainQuestionCategories = questionCategoryRepository.getMainQuestionCategories();
+        return QuestionCategoryGroupBySubject.create(mainQuestionCategories);
     }
 }
