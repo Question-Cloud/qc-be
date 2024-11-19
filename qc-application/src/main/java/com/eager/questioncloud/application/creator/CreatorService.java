2dcfd1c5b2853b286e7eee1a26482c3b6455f0c5
e4g3r <qaws5656@naver.com>
2024-11-19 18:20:53 +0900

    refactor: 불필요한 구현체 레이어 제거

--- qc-application/src/main/java/com/eager/questioncloud/application/creator/CreatorService.java
+++ qc-application/src/main/java/com/eager/questioncloud/application/creator/CreatorService.java
@@ -1,15 +1,16 @@
 package com.eager.questioncloud.application.creator;
 
 import com.eager.questioncloud.domain.creator.CreatorInformation;
+import com.eager.questioncloud.domain.creator.CreatorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CreatorService {
-    private final CreatorReader creatorReader;
+    private final CreatorRepository creatorRepository;
 
     public CreatorInformation getCreatorInformation(Long creatorId) {
-        return creatorReader.getCreatorInformation(creatorId);
+        return creatorRepository.getCreatorInformation(creatorId);
     }
 }
