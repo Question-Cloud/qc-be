01385da8abd253e43ab78fa6c9fa44060e6e6fa0
e4g3r <qaws5656@naver.com>
2024-11-19 20:05:37 +0900

    refactor: QuestionHubReview 불필요 구현체 레이어 제거

--- qc-application/src/main/java/com/eager/questioncloud/application/hub/QuestionHubReviewService.java
+++ qc-application/src/main/java/com/eager/questioncloud/application/hub/QuestionHubReviewService.java
@@ -4,6 +4,7 @@ import com.eager.questioncloud.common.PagingInformation;
 import com.eager.questioncloud.domain.review.MyQuestionReview;
 import com.eager.questioncloud.domain.review.QuestionReview;
 import com.eager.questioncloud.domain.review.QuestionReviewDetail;
+import com.eager.questioncloud.domain.review.QuestionReviewRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
@@ -12,45 +13,43 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class QuestionHubReviewService {
-    private final QuestionHubReviewReader questionHubReviewReader;
+    private final QuestionReviewRepository questionReviewRepository;
     private final QuestionHubReviewRegister questionHubReviewRegister;
-    private final QuestionHubReviewUpdater questionHubReviewUpdater;
-    private final QuestionHubReviewRemover questionHubReviewRemover;
     private final ApplicationEventPublisher applicationEventPublisher;
 
     public int getTotal(Long questionId) {
-        return questionHubReviewReader.getTotal(questionId);
+        return questionReviewRepository.getTotal(questionId);
     }
 
     public List<QuestionReviewDetail> getQuestionReviews(Long questionId, Long userId, PagingInformation pagingInformation) {
-        return questionHubReviewReader.getQuestionReviews(questionId, userId, pagingInformation);
+        return questionReviewRepository.getQuestionReviews(questionId, userId, pagingInformation);
     }
 
     public MyQuestionReview getMyQuestionReview(Long questionId, Long userId) {
-        return questionHubReviewReader.getMyQuestionReview(questionId, userId);
+        QuestionReview questionReview = questionReviewRepository.getMyQuestionReview(questionId, userId);
+        return MyQuestionReview.from(questionReview);
     }
 
     //TODO Event 처리
     public void register(QuestionReview questionReview) {
         questionHubReviewRegister.register(questionReview);
-
 //        applicationEventPublisher.publishEvent(RegisteredReviewEvent.create(questionReview.getQuestionId(), questionReview.getRate()));
     }
 
     //TODO Event 처리
     public void modify(Long reviewId, Long userId, String comment, int rate) {
-        QuestionReview questionReview = questionHubReviewReader.findByIdAndUserId(reviewId, userId);
+        QuestionReview questionReview = questionReviewRepository.findByIdAndUserId(reviewId, userId);
         int varianceRate = rate - questionReview.getRate();
-        questionHubReviewUpdater.update(questionReview, comment, rate);
-
+        questionReview.modify(comment, rate);
+        questionReviewRepository.save(questionReview);
 //        applicationEventPublisher.publishEvent(ModifiedReviewEvent.create(questionReview.getQuestionId(), varianceRate));
     }
 
     //TODO Event 처리
     public void delete(Long reviewId, Long userId) {
-        QuestionReview questionReview = questionHubReviewReader.findByIdAndUserId(reviewId, userId);
-        questionHubReviewRemover.delete(questionReview);
-
+        QuestionReview questionReview = questionReviewRepository.findByIdAndUserId(reviewId, userId);
+        questionReview.delete();
+        questionReviewRepository.save(questionReview);
 //        applicationEventPublisher.publishEvent(DeletedReviewEvent.create(questionReview.getQuestionId(), questionReview.getRate()));
     }
 }
