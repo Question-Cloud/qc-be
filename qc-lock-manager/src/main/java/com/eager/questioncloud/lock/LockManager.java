a73964d611a4a20f33cd8361ccad71c9ea4297a3
e4g3r <qaws5656@naver.com>
2024-12-14 15:05:30 +0900

    feat: LockManager executeWithLock catch 수정

--- qc-lock-manager/src/main/java/com/eager/questioncloud/lock/LockManager.java
+++ qc-lock-manager/src/main/java/com/eager/questioncloud/lock/LockManager.java
@@ -26,16 +26,16 @@ public class LockManager {
                 throw new CustomException(Error.INTERNAL_SERVER_ERROR);
             }
             task.run();
-        } catch (InterruptedException e) {
-            if (lock.isLocked() && lock.isHeldByCurrentThread()) {
-                lock.unlock();
-            }
-            throw new CustomException(Error.INTERNAL_SERVER_ERROR);
         } catch (CustomException e) {
             if (lock.isLocked() && lock.isHeldByCurrentThread()) {
                 lock.unlock();
             }
             throw e;
+        } catch (Exception e) {
+            if (lock.isLocked() && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+            throw new CustomException(Error.INTERNAL_SERVER_ERROR);
         } finally {
             if (lock.isLocked() && lock.isHeldByCurrentThread()) {
                 lock.unlock();
