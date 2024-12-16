74ccf5cf9a961985c981da26598ec912a669dae4
e4g3r <qaws5656@naver.com>
2024-12-16 20:26:42 +0900

    refactor: QuestionPaymentHistory 저장은 이벤트 발행을 통해 처리하도록 변경

--- qc-application/src/main/java/com/eager/questioncloud/application/api/payment/question/service/QuestionPaymentService.java
+++ qc-application/src/main/java/com/eager/questioncloud/application/api/payment/question/service/QuestionPaymentService.java
@@ -3,7 +3,6 @@ package com.eager.questioncloud.application.api.payment.question.service;
 import com.eager.questioncloud.application.api.payment.question.implement.QuestionOrderGenerator;
 import com.eager.questioncloud.application.api.payment.question.implement.QuestionPaymentProcessor;
 import com.eager.questioncloud.core.domain.payment.event.CompletedQuestionPaymentEvent;
-import com.eager.questioncloud.core.domain.payment.implement.QuestionPaymentHistoryGenerator;
 import com.eager.questioncloud.core.domain.payment.model.QuestionOrder;
 import com.eager.questioncloud.core.domain.payment.model.QuestionPayment;
 import java.util.List;
@@ -16,13 +15,11 @@ import org.springframework.stereotype.Service;
 public class QuestionPaymentService {
     private final QuestionPaymentProcessor questionPaymentProcessor;
     private final QuestionOrderGenerator questionOrderGenerator;
-    private final QuestionPaymentHistoryGenerator questionPaymentHistoryGenerator;
     private final ApplicationEventPublisher applicationEventPublisher;
 
     public void payment(Long userId, List<Long> questionIds, Long userCouponId) {
         QuestionOrder order = questionOrderGenerator.generateQuestionPaymentOrder(userId, questionIds);
         QuestionPayment paymentResult = questionPaymentProcessor.processQuestionPayment(QuestionPayment.create(userId, userCouponId, order));
-        questionPaymentHistoryGenerator.saveQuestionPaymentHistory(paymentResult, questionIds);
         applicationEventPublisher.publishEvent(CompletedQuestionPaymentEvent.create(paymentResult, questionIds));
     }
 }
--- qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/implement/QuestionPaymentHistoryGenerator.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/implement/QuestionPaymentHistoryGenerator.java
@@ -4,13 +4,15 @@ import com.eager.questioncloud.core.domain.coupon.infrastructure.CouponRepositor
 import com.eager.questioncloud.core.domain.coupon.infrastructure.UserCouponRepository;
 import com.eager.questioncloud.core.domain.coupon.model.Coupon;
 import com.eager.questioncloud.core.domain.coupon.model.UserCoupon;
+import com.eager.questioncloud.core.domain.payment.event.CompletedQuestionPaymentEvent;
 import com.eager.questioncloud.core.domain.payment.infrastructure.QuestionPaymentHistoryRepository;
-import com.eager.questioncloud.core.domain.payment.model.QuestionPayment;
 import com.eager.questioncloud.core.domain.payment.model.QuestionPaymentHistory;
 import com.eager.questioncloud.core.domain.question.dto.QuestionInformation;
 import com.eager.questioncloud.core.domain.question.infrastructure.QuestionRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,15 +23,17 @@ public class QuestionPaymentHistoryGenerator {
     private final UserCouponRepository userCouponRepository;
     private final QuestionPaymentHistoryRepository questionPaymentHistoryRepository;
 
-    public void saveQuestionPaymentHistory(QuestionPayment questionPayment, List<Long> questionIds) {
-        List<QuestionInformation> questions = questionRepository.findByQuestionIdIn(questionIds);
+    @EventListener
+    @Async
+    public void saveQuestionPaymentHistory(CompletedQuestionPaymentEvent event) {
+        List<QuestionInformation> questions = questionRepository.findByQuestionIdIn(event.getQuestionIds());
 
-        if (questionPayment.isUsingCoupon()) {
-            UserCoupon userCoupon = userCouponRepository.getUserCoupon(questionPayment.getUserCouponId());
+        if (event.getQuestionPayment().isUsingCoupon()) {
+            UserCoupon userCoupon = userCouponRepository.getUserCoupon(event.getQuestionPayment().getUserCouponId());
             Coupon coupon = couponRepository.findById(userCoupon.getCouponId());
-            questionPaymentHistoryRepository.save(QuestionPaymentHistory.create(questionPayment, questions, coupon));
+            questionPaymentHistoryRepository.save(QuestionPaymentHistory.create(event.getQuestionPayment(), questions, coupon));
         }
 
-        questionPaymentHistoryRepository.save(QuestionPaymentHistory.create(questionPayment, questions, null));
+        questionPaymentHistoryRepository.save(QuestionPaymentHistory.create(event.getQuestionPayment(), questions, null));
     }
 }
