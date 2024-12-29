2205e5596be3c99ea9f78d6a28e2df43e5b68da8
e4g3r <qaws5656@naver.com>
2024-12-29 19:19:40 +0900

    feat: rabbit mq rabbitTemplate 추가

--- qc-application/src/main/java/com/eager/questioncloud/application/config/RabbitConfig.java
+++ qc-application/src/main/java/com/eager/questioncloud/application/config/RabbitConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +26,17 @@ public class RabbitConfig {
     @Bean
     public Binding bindingFailChargePoint(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with("fail-charge-point");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+    
+    @Bean
+    public MessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
