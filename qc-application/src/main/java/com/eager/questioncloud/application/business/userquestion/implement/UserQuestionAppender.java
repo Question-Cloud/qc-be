01abade5f829b33604feaf3eeb6381d288b3ad01
e4g3r <qaws5656@naver.com>
2025-02-06 01:45:09 +0900

    refactor: 문제 구매 후처리 로직 분리 (나의 문제집 추가)  - QuestionPaymentPostProcessor에 있는 기존 로직 UserQuestionAppender 분리

---
+++ qc-application/src/main/java/com/eager/questioncloud/application/business/userquestion/implement/UserQuestionAppender.java
@@ -0,0 +1,35 @@
+package com.eager.questioncloud.application.business.userquestion.implement;
+
+import com.eager.questioncloud.application.business.payment.question.event.QuestionPaymentEvent;
+import com.eager.questioncloud.application.message.FailQuestionPaymentMessage;
+import com.eager.questioncloud.application.message.MessageSender;
+import com.eager.questioncloud.application.message.MessageType;
+import com.eager.questioncloud.core.domain.userquestion.infrastructure.repository.UserQuestionRepository;
+import com.eager.questioncloud.core.domain.userquestion.model.UserQuestion;
+import com.eager.questioncloud.core.exception.CoreException;
+import com.eager.questioncloud.core.exception.Error;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserQuestionAppender {
+    private final UserQuestionRepository userQuestionRepository;
+    private final MessageSender messageSender;
+    
+    @EventListener
+    public void appendUserQuestion(QuestionPaymentEvent event) {
+        try {
+            userQuestionRepository.saveAll(
+                UserQuestion.create(
+                    event.getQuestionPayment().getUserId(),
+                    event.getQuestionPayment().getOrder().getQuestionIds()
+                )
+            );
+        } catch (Exception e) {
+            messageSender.sendMessage(MessageType.FAIL_QUESTION_PAYMENT, new FailQuestionPaymentMessage(event.getQuestionPayment()));
+            throw new CoreException(Error.PAYMENT_ERROR);
+        }
+    }
+}
--- qc-application/src/main/java/com/eager/questioncloud/application/business/userquestion/service/UserQuestionService.java
+++ qc-application/src/main/java/com/eager/questioncloud/application/business/userquestion/service/UserQuestionService.java
@@ -1,10 +1,13 @@
 package com.eager.questioncloud.application.business.userquestion.service;
 
+import com.eager.questioncloud.application.business.payment.question.event.QuestionPaymentEvent;
 import com.eager.questioncloud.core.domain.question.common.QuestionFilter;
 import com.eager.questioncloud.core.domain.userquestion.dto.UserQuestionDetail;
 import com.eager.questioncloud.core.domain.userquestion.infrastructure.repository.UserQuestionRepository;
+import com.eager.questioncloud.core.domain.userquestion.model.UserQuestion;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +21,14 @@ public class UserQuestionService {
 
     public int countUserQuestions(QuestionFilter questionFilter) {
         return userQuestionRepository.countUserQuestions(questionFilter);
+    }
+
+    @EventListener
+    public void appendUserQuestion(QuestionPaymentEvent questionPaymentEvent) {
+        userQuestionRepository.saveAll(
+            UserQuestion.create(
+                questionPaymentEvent.getQuestionPayment().getUserId(),
+                questionPaymentEvent.getQuestionPayment().getOrder().getQuestionIds())
+        );
     }
 }
