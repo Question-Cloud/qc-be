2ce7ea204f9f4c58dd31b77b7671748e3d893f9b
e4g3r <qaws5656@naver.com>
2025-03-01 22:23:59 +0900

    refactor: 미사용 메서드 제거

--- qc-lock-manager/src/main/java/com/eager/questioncloud/lock/LockKeyGenerator.java
+++ qc-lock-manager/src/main/java/com/eager/questioncloud/lock/LockKeyGenerator.java
@@ -5,18 +5,6 @@ public class LockKeyGenerator {
         return "REGISTER-COUPON-" + userId + "-" + couponCode;
     }
 
-    public static String generateChargePointPaymentKey(String paymentId) {
-        return "CHARGE-POINT-PAYMENT-" + paymentId;
-    }
-
-    public static String generateQuestionPaymentKey(Long userId) {
-        return "QUESTION-PAYMENT-" + userId;
-    }
-
-    public static String generateRegisterReview(Long userId, Long questionId) {
-        return "REGISTER-REVIEW-" + userId + "-" + questionId;
-    }
-
     public static String generateCreatorStatistics(Long creatorId) {
         return "CREATOR-STATISTICS-" + creatorId;
     }
