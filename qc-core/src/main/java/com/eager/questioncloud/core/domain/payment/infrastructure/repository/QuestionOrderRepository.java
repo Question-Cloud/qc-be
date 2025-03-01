f3358ede00f5f3e7177cc6fcb01793b831aa07fd
e4g3r <qaws5656@naver.com>
2025-03-01 14:24:11 +0900

    refactor: 미사용 메서드 제거

--- qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/infrastructure/repository/QuestionOrderRepository.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/infrastructure/repository/QuestionOrderRepository.java
@@ -1,11 +1,8 @@
 package com.eager.questioncloud.core.domain.payment.infrastructure.repository;
 
 import com.eager.questioncloud.core.domain.payment.model.QuestionOrder;
-import java.util.List;
 
 public interface QuestionOrderRepository {
-    Boolean isAlreadyPurchased(Long userId, List<Long> questionIds);
-
     void save(QuestionOrder questionOrder);
 
     void deleteAllInBatch();
--- qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/infrastructure/repository/QuestionOrderRepositoryImpl.java
+++ qc-core/src/main/java/com/eager/questioncloud/core/domain/payment/infrastructure/repository/QuestionOrderRepositoryImpl.java
@@ -1,12 +1,7 @@
 package com.eager.questioncloud.core.domain.payment.infrastructure.repository;
 
-import static com.eager.questioncloud.core.domain.payment.infrastructure.entity.QQuestionOrderEntity.questionOrderEntity;
-import static com.eager.questioncloud.core.domain.payment.infrastructure.entity.QQuestionPaymentEntity.questionPaymentEntity;
-
 import com.eager.questioncloud.core.domain.payment.infrastructure.entity.QuestionOrderEntity;
 import com.eager.questioncloud.core.domain.payment.model.QuestionOrder;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -14,17 +9,6 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class QuestionOrderRepositoryImpl implements QuestionOrderRepository {
     private final QuestionOrderJpaRepository questionOrderJpaRepository;
-    private final JPAQueryFactory jpaQueryFactory;
-
-    @Override
-    public Boolean isAlreadyPurchased(Long userId, List<Long> questionIds) {
-        return jpaQueryFactory.select(questionOrderEntity.id, questionPaymentEntity.userId)
-            .from(questionOrderEntity)
-            .leftJoin(questionPaymentEntity)
-            .on(questionOrderEntity.orderId.eq(questionPaymentEntity.orderId), questionPaymentEntity.userId.eq(userId))
-            .where(questionOrderEntity.questionId.in(questionIds))
-            .fetchFirst() != null;
-    }
 
     @Override
     public void save(QuestionOrder questionOrder) {
