ea7d7544c8db52a1d51ed458b74bd2e4365663ab
e4g3r <qaws5656@naver.com>
2025-03-05 01:18:36 +0900

    refactor: qc-application 유저 포인트 관련 로직 코틀린 전환

--- qc-application/src/main/java/com/eager/questioncloud/application/business/point/service/UserPointService.kt
+++ qc-application/src/main/java/com/eager/questioncloud/application/business/point/service/UserPointService.kt
@@ -1,16 +1,14 @@
-package com.eager.questioncloud.application.business.point.service;
+package com.eager.questioncloud.application.business.point.service
 
-import com.eager.questioncloud.core.domain.point.infrastructure.repository.UserPointRepository;
-import com.eager.questioncloud.core.domain.point.model.UserPoint;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.eager.questioncloud.core.domain.point.infrastructure.repository.UserPointRepository
+import com.eager.questioncloud.core.domain.point.model.UserPoint
+import org.springframework.stereotype.Service
 
 @Service
-@RequiredArgsConstructor
-public class UserPointService {
-    private final UserPointRepository userPointRepository;
-
-    public UserPoint getUserPoint(Long userId) {
-        return userPointRepository.getUserPoint(userId);
+class UserPointService(
+    private val userPointRepository: UserPointRepository
+) {
+    fun getUserPoint(userId: Long): UserPoint {
+        return userPointRepository.getUserPoint(userId)
     }
 }
