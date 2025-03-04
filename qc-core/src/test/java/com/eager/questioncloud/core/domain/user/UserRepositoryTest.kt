89df1fd70c7e982ff2c92092388bc7722b9d060d
e4g3r <qaws5656@naver.com>
2025-03-04 14:15:27 +0900

    test: qc-core UserTest 코틀린 전환

--- qc-core/src/test/java/com/eager/questioncloud/core/domain/user/UserRepositoryTest.kt
+++ qc-core/src/test/java/com/eager/questioncloud/core/domain/user/UserRepositoryTest.kt
@@ -1,45 +1,51 @@
-package com.eager.questioncloud.core.domain.user;
+package com.eager.questioncloud.core.domain.user
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.eager.questioncloud.core.domain.user.dto.CreateUser;
-import com.eager.questioncloud.core.domain.user.enums.AccountType;
-import com.eager.questioncloud.core.domain.user.enums.UserStatus;
-import com.eager.questioncloud.core.domain.user.enums.UserType;
-import com.eager.questioncloud.core.domain.user.infrastructure.repository.UserRepository;
-import com.eager.questioncloud.core.domain.user.model.User;
-import com.eager.questioncloud.core.domain.user.model.UserAccountInformation;
-import com.eager.questioncloud.core.domain.user.model.UserInformation;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
+import com.eager.questioncloud.core.domain.user.dto.CreateUser
+import com.eager.questioncloud.core.domain.user.enums.AccountType
+import com.eager.questioncloud.core.domain.user.enums.UserStatus
+import com.eager.questioncloud.core.domain.user.enums.UserType
+import com.eager.questioncloud.core.domain.user.infrastructure.repository.UserRepository
+import com.eager.questioncloud.core.domain.user.model.User.Companion.create
+import com.eager.questioncloud.core.domain.user.model.UserAccountInformation.Companion.createEmailAccountInformation
+import com.eager.questioncloud.core.domain.user.model.UserInformation.Companion.create
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
-class UserRepositoryTest {
+internal class UserRepositoryTest(
     @Autowired
-    private UserRepository userRepository;
-
+    private val userRepository: UserRepository
+) {
     @Test
     @DisplayName("이메일이 일치하는 유저를 조회한다.")
-    void 이메일이_일치하는_유저를_조회한다() {
+    fun 이메일이_일치하는_유저를_조회한다() {
         //given
-        String email = "test@test.com";
-        String password = "qwer1234";
+        val email = "test@test.com"
+        val password = "qwer1234"
 
-        CreateUser createUser = new CreateUser(email, password, null, AccountType.EMAIL, "01012345678", "김승환");
-        UserAccountInformation userAccountInformation = UserAccountInformation.createEmailAccountInformation(password);
-        UserInformation userInformation = UserInformation.create(createUser);
-        User savedUser = userRepository.save(User.create(userAccountInformation, userInformation, UserType.NormalUser, UserStatus.Active));
+        val createUser = CreateUser(email, password, null, AccountType.EMAIL, "01012345678", "김승환")
+        val userAccountInformation = createEmailAccountInformation(password)
+        val userInformation = create(createUser)
+        userRepository.save(
+            create(
+                userAccountInformation,
+                userInformation,
+                UserType.NormalUser,
+                UserStatus.Active
+            )
+        )
 
         //when
-        User user = userRepository.getUserByEmail(email);
+        val user = userRepository.getUserByEmail(email)
 
         //then
-        assertThat(user.getUserInformation().getEmail()).isEqualTo(email);
+        Assertions.assertThat(user.userInformation.email).isEqualTo(email)
     }
 }
--- qc-core/src/test/java/com/eager/questioncloud/core/domain/user/UserTest.kt
+++ qc-core/src/test/java/com/eager/questioncloud/core/domain/user/UserTest.kt
@@ -1,103 +1,97 @@
-package com.eager.questioncloud.core.domain.user;
+package com.eager.questioncloud.core.domain.user
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import com.eager.questioncloud.core.domain.user.enums.UserStatus
+import com.eager.questioncloud.core.domain.user.model.User
+import com.eager.questioncloud.core.domain.user.model.UserAccountInformation.Companion.createEmailAccountInformation
+import com.eager.questioncloud.core.exception.CoreException
+import com.eager.questioncloud.core.exception.Error
+import com.eager.questioncloud.core.exception.NotVerificationUserException
+import com.eager.questioncloud.utils.Fixture
+import com.navercorp.fixturemonkey.kotlin.giveMeKotlinBuilder
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 
-import com.eager.questioncloud.core.domain.user.dto.CreateUser;
-import com.eager.questioncloud.core.domain.user.enums.AccountType;
-import com.eager.questioncloud.core.domain.user.enums.UserStatus;
-import com.eager.questioncloud.core.domain.user.enums.UserType;
-import com.eager.questioncloud.core.domain.user.model.User;
-import com.eager.questioncloud.core.domain.user.model.UserAccountInformation;
-import com.eager.questioncloud.core.domain.user.model.UserInformation;
-import com.eager.questioncloud.core.exception.CoreException;
-import com.eager.questioncloud.core.exception.Error;
-import com.eager.questioncloud.core.exception.NotVerificationUserException;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-class UserTest {
+internal class UserTest {
     @Test
     @DisplayName("비밀번호를 검사하고 일치하면 예외를 발생시키지 않는다.")
-    void 비밀번호를_검사하고_일치하면_예외를_발생시키지_않는다() {
+    fun 비밀번호를_검사하고_일치하면_예외를_발생시키지_않는다() {
         //given
-        String email = "test@test.com";
-        String password = "qwer1234";
-        CreateUser createUser = new CreateUser(email, password, null, AccountType.EMAIL, "01012345678", "김승환");
-        UserAccountInformation userAccountInformation = UserAccountInformation.createEmailAccountInformation(password);
-        UserInformation userInformation = UserInformation.create(createUser);
-        User user = User.create(userAccountInformation, userInformation, UserType.NormalUser, UserStatus.Active);
+        val password = "qwer1234"
+        val user = Fixture.fixtureMonkey.giveMeKotlinBuilder<User>()
+            .set(User::userAccountInformation, createEmailAccountInformation(password))
+            .set(User::uid, 1L)
+            .build()
+            .sample()
 
         //when then
-        assertThatCode(() -> user.passwordAuthentication(password))
-            .doesNotThrowAnyException();
+        Assertions.assertThatCode { user.passwordAuthentication(password) }
+            .doesNotThrowAnyException()
     }
 
     @Test
     @DisplayName("비밀번호가 일치하지 않다면 로그인 실패 예외를 던진다.")
-    void 비밀번호가_일치하지_않다면_로그인_실패_예외를_던진다() {
+    fun 비밀번호가_일치하지_않다면_로그인_실패_예외를_던진다() {
         //given
-        String email = "test@test.com";
-        String password = "qwer1234";
-        String wrongPassword = "qwer1235";
-        CreateUser createUser = new CreateUser(email, password, null, AccountType.EMAIL, "01012345678", "김승환");
-        UserAccountInformation userAccountInformation = UserAccountInformation.createEmailAccountInformation(password);
-        UserInformation userInformation = UserInformation.create(createUser);
-        User user = User.create(userAccountInformation, userInformation, UserType.NormalUser, UserStatus.Active);
+        val password = "qwer1234"
+        val wrongPassword = "qwer1235"
+
+        val user = Fixture.fixtureMonkey.giveMeKotlinBuilder<User>()
+            .set(User::userAccountInformation, createEmailAccountInformation(password))
+            .set(User::uid, 1L)
+            .build()
+            .sample()
 
         //when then
-        assertThatThrownBy(() -> user.passwordAuthentication(wrongPassword))
-            .isInstanceOf(CoreException.class)
-            .hasFieldOrPropertyWithValue("error", Error.FAIL_LOGIN);
+        Assertions.assertThatThrownBy { user.passwordAuthentication(wrongPassword) }
+            .isInstanceOf(CoreException::class.java)
+            .hasFieldOrPropertyWithValue("error", Error.FAIL_LOGIN)
     }
 
     @Test
     @DisplayName("이메일 미인증 유저라면 PendingEmailVerification 예외를 던진다.")
-    void 이메일_미인증_유저라면_PendingEmailVerification_예외를_던진다() {
+    fun 이메일_미인증_유저라면_PendingEmailVerification_예외를_던진다() {
         //given
-        String email = "test@test.com";
-        String password = "qwer1234";
-        CreateUser createUser = new CreateUser(email, password, null, AccountType.EMAIL, "01012345678", "김승환");
-        UserAccountInformation userAccountInformation = UserAccountInformation.createEmailAccountInformation(password);
-        UserInformation userInformation = UserInformation.create(createUser);
-        User user = User.create(userAccountInformation, userInformation, UserType.NormalUser, UserStatus.PendingEmailVerification);
+        val user = Fixture.fixtureMonkey.giveMeKotlinBuilder<User>()
+            .set(User::uid, 1L)
+            .set(User::userStatus, UserStatus.PendingEmailVerification)
+            .build()
+            .sample()
 
         //when then
-        assertThatThrownBy(user::checkUserStatus)
-            .isInstanceOf(NotVerificationUserException.class);
+        Assertions.assertThatThrownBy { user.checkUserStatus() }
+            .isInstanceOf(NotVerificationUserException::class.java)
     }
 
     @Test
     @DisplayName("정지 된 유저라면 NOT_ACTIVE_USER 예외를 던진다.")
-    void 정지_된_유저라면_NOT_ACTIVE_USER_예외를_던진다() {
+    fun 정지_된_유저라면_NOT_ACTIVE_USER_예외를_던진다() {
         //given
-        String email = "test@test.com";
-        String password = "qwer1234";
-        CreateUser createUser = new CreateUser(email, password, null, AccountType.EMAIL, "01012345678", "김승환");
-        UserAccountInformation userAccountInformation = UserAccountInformation.createEmailAccountInformation(password);
-        UserInformation userInformation = UserInformation.create(createUser);
-        User user = User.create(userAccountInformation, userInformation, UserType.NormalUser, UserStatus.Ban);
+        val user = Fixture.fixtureMonkey.giveMeKotlinBuilder<User>()
+            .set(User::uid, 1L)
+            .set(User::userStatus, UserStatus.Ban)
+            .build()
+            .sample()
 
         //when then
-        assertThatThrownBy(user::checkUserStatus)
-            .isInstanceOf(CoreException.class)
-            .hasFieldOrPropertyWithValue("error", Error.NOT_ACTIVE_USER);
+        Assertions.assertThatThrownBy { user.checkUserStatus() }
+            .isInstanceOf(CoreException::class.java)
+            .hasFieldOrPropertyWithValue("error", Error.NOT_ACTIVE_USER)
     }
 
     @Test
     @DisplayName("탈퇴 유저라면 NOT_ACTIVE_USER 예외를 던진다.")
-    void 탈퇴_유저라면_NOT_ACTIVE_USER_예외를_던진다() {
+    fun 탈퇴_유저라면_NOT_ACTIVE_USER_예외를_던진다() {
         //given
-        String email = "test@test.com";
-        String password = "qwer1234";
-        CreateUser createUser = new CreateUser(email, password, null, AccountType.EMAIL, "01012345678", "김승환");
-        UserAccountInformation userAccountInformation = UserAccountInformation.createEmailAccountInformation(password);
-        UserInformation userInformation = UserInformation.create(createUser);
-        User user = User.create(userAccountInformation, userInformation, UserType.NormalUser, UserStatus.Deleted);
+        val user = Fixture.fixtureMonkey.giveMeKotlinBuilder<User>()
+            .set(User::uid, 1L)
+            .set(User::userStatus, UserStatus.Deleted)
+            .build()
+            .sample()
 
         //when then
-        assertThatThrownBy(user::checkUserStatus)
-            .isInstanceOf(CoreException.class)
-            .hasFieldOrPropertyWithValue("error", Error.NOT_ACTIVE_USER);
+        Assertions.assertThatThrownBy { user.checkUserStatus() }
+            .isInstanceOf(CoreException::class.java)
+            .hasFieldOrPropertyWithValue("error", Error.NOT_ACTIVE_USER)
     }
 }
