2248bd8221c622e40fe66b8be740c871a46e731b
e4g3r <qaws5656@naver.com>
2025-03-05 01:41:07 +0900

    refactor: qc-application EmailSender 코틀린 전환

--- qc-application/src/main/java/com/eager/questioncloud/application/mail/EmailSender.kt
+++ qc-application/src/main/java/com/eager/questioncloud/application/mail/EmailSender.kt
@@ -1,33 +1,23 @@
-package com.eager.questioncloud.application.mail;
+package com.eager.questioncloud.application.mail
 
-import com.eager.questioncloud.core.domain.verification.model.Email;
-import com.eager.questioncloud.core.exception.CoreException;
-import com.eager.questioncloud.core.exception.Error;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
+import com.eager.questioncloud.core.domain.verification.model.Email
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
 
 @Component
-@RequiredArgsConstructor
-public class EmailSender {
-    private final JavaMailSender javaMailSender;
-
+class EmailSender(
+    private val javaMailSender: JavaMailSender
+) {
     @Async
-    public void sendMail(Email email) {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = null;
-        try {
-            mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-            mimeMessageHelper.setTo(email.getTo());
-            mimeMessageHelper.setSubject(email.getSubject());
-            mimeMessageHelper.setText(email.getContent(), true);
-            javaMailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            throw new CoreException(Error.INTERNAL_SERVER_ERROR);
-        }
+    fun sendMail(email: Email) {
+        val mimeMessage = javaMailSender.createMimeMessage()
+        val mimeMessageHelper = MimeMessageHelper(mimeMessage, false, "UTF-8")
+
+        mimeMessageHelper.setTo(email.to)
+        mimeMessageHelper.setSubject(email.subject)
+        mimeMessageHelper.setText(email.content, true)
+        javaMailSender.send(mimeMessage)
     }
 }
