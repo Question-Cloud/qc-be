71a89c03e4c8c0928fa5e7e51dd07c392f0b32f6
e4g3r <qaws5656@naver.com>
2025-03-05 19:45:39 +0900

    refactor: qc-application Feed API 클래스 코틀린 전환

--- qc-application/src/main/java/com/eager/questioncloud/application/api/feed/library/controller/FeedLibraryController.kt
+++ qc-application/src/main/java/com/eager/questioncloud/application/api/feed/library/controller/FeedLibraryController.kt
@@ -1,36 +1,32 @@
-package com.eager.questioncloud.application.api.feed.library.controller;
+package com.eager.questioncloud.application.api.feed.library.controller
 
-import com.eager.questioncloud.application.api.common.PagingResponse;
-import com.eager.questioncloud.application.business.userquestion.service.UserQuestionService;
-import com.eager.questioncloud.core.domain.question.common.QuestionFilter;
-import com.eager.questioncloud.core.domain.userquestion.dto.UserQuestionDetail;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eager.questioncloud.application.api.common.PagingResponse
+import com.eager.questioncloud.application.business.userquestion.service.UserQuestionService
+import com.eager.questioncloud.core.domain.question.common.QuestionFilter
+import com.eager.questioncloud.core.domain.userquestion.dto.UserQuestionDetail
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springdoc.core.annotations.ParameterObject
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/feed/library")
-@RequiredArgsConstructor
-public class FeedLibraryController {
-    private final UserQuestionService userQuestionService;
-
+class FeedLibraryController(
+    private val userQuestionService: UserQuestionService
+) {
     @GetMapping
-    @ApiResponses(value = {
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
-    })
-    @Operation(operationId = "나의 문제 목록 조회", summary = "나의 문제 목록 조회", tags = {"library"}, description = "나의 문제 목록 조회")
-    @Parameter(name = "size", description = "paging size", schema = @Schema(type = "integer"))
-    @Parameter(name = "page", description = "paging page", schema = @Schema(type = "integer"))
-    public PagingResponse<UserQuestionDetail> getUserQuestionLibraryList(@ParameterObject QuestionFilter questionFilter) {
-        int total = userQuestionService.countUserQuestions(questionFilter);
-        List<UserQuestionDetail> userQuestions = userQuestionService.getUserQuestions(questionFilter);
-        return new PagingResponse<>(total, userQuestions);
+    @ApiResponses(value = [ApiResponse(responseCode = "200", description = "요청 성공")])
+    @Operation(operationId = "나의 문제 목록 조회", summary = "나의 문제 목록 조회", tags = ["library"], description = "나의 문제 목록 조회")
+    @Parameter(name = "size", description = "paging size", schema = Schema(type = "integer"))
+    @Parameter(name = "page", description = "paging page", schema = Schema(type = "integer"))
+    fun getUserQuestionLibraryList(@ParameterObject questionFilter: QuestionFilter): PagingResponse<UserQuestionDetail> {
+        val total = userQuestionService.countUserQuestions(questionFilter)
+        val userQuestions = userQuestionService.getUserQuestions(questionFilter)
+        return PagingResponse(total, userQuestions)
     }
 }
--- qc-application/src/main/java/com/eager/questioncloud/application/api/feed/subscribe/controller/FeedSubscribeController.kt
+++ qc-application/src/main/java/com/eager/questioncloud/application/api/feed/subscribe/controller/FeedSubscribeController.kt
@@ -1,38 +1,37 @@
-package com.eager.questioncloud.application.api.feed.subscribe.controller;
+package com.eager.questioncloud.application.api.feed.subscribe.controller
 
-import com.eager.questioncloud.application.api.common.PagingResponse;
-import com.eager.questioncloud.application.business.subscribe.service.FeedSubscribeService;
-import com.eager.questioncloud.application.security.UserPrincipal;
-import com.eager.questioncloud.core.common.PagingInformation;
-import com.eager.questioncloud.core.domain.subscribe.dto.SubscribeDetail;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eager.questioncloud.application.api.common.PagingResponse
+import com.eager.questioncloud.application.business.subscribe.service.FeedSubscribeService
+import com.eager.questioncloud.application.security.UserPrincipal
+import com.eager.questioncloud.core.common.PagingInformation
+import com.eager.questioncloud.core.domain.subscribe.dto.SubscribeDetail
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/feed/subscribe")
-@RequiredArgsConstructor
-public class FeedSubscribeController {
-    private final FeedSubscribeService feedSubscribeService;
-
+class FeedSubscribeController(
+    private val feedSubscribeService: FeedSubscribeService
+) {
     @GetMapping("/my-subscribe")
-    @ApiResponses(value = {
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "요청 성공")
-    })
-    @Operation(operationId = "나의 구독 목록 조회", summary = "나의 구독 목록 조회", tags = {"subscribe"}, description = "나의 구독 목록 조회")
-    @Parameter(name = "size", description = "paging size", schema = @Schema(type = "integer"))
-    @Parameter(name = "page", description = "paging page", schema = @Schema(type = "integer"))
-    public PagingResponse<SubscribeDetail> getMySubscribeList(
-        @AuthenticationPrincipal UserPrincipal userPrincipal, PagingInformation pagingInformation) {
-        int total = feedSubscribeService.countMySubscribe(userPrincipal.getUser().getUid());
-        List<SubscribeDetail> subscribeCreators = feedSubscribeService.getMySubscribes(userPrincipal.getUser().getUid(), pagingInformation);
-        return new PagingResponse<>(total, subscribeCreators);
+    @ApiResponses(value = [ApiResponse(responseCode = "200", description = "요청 성공")])
+    @Operation(operationId = "나의 구독 목록 조회", summary = "나의 구독 목록 조회", tags = ["subscribe"], description = "나의 구독 목록 조회")
+    @Parameter(name = "size", description = "paging size", schema = Schema(type = "integer"))
+    @Parameter(name = "page", description = "paging page", schema = Schema(type = "integer"))
+    fun getMySubscribeList(
+        @AuthenticationPrincipal userPrincipal: UserPrincipal, pagingInformation: PagingInformation
+    ): PagingResponse<SubscribeDetail> {
+        val total = feedSubscribeService.countMySubscribe(userPrincipal.user.uid!!)
+        val subscribeCreators = feedSubscribeService.getMySubscribes(
+            userPrincipal.user.uid!!, pagingInformation
+        )
+        return PagingResponse(total, subscribeCreators)
     }
 }
