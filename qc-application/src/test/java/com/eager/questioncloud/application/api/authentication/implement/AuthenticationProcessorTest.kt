febcf061e2088799d5f4be1975fbe1577056f0c7
e4g3r <qaws5656@naver.com>
2025-03-14 23:03:06 +0900

    test: AuthenticationProcessorTest 테스트 코드 리팩토링

--- qc-application/src/test/java/com/eager/questioncloud/application/api/authentication/implement/AuthenticationProcessorTest.kt
+++ qc-application/src/test/java/com/eager/questioncloud/application/api/authentication/implement/AuthenticationProcessorTest.kt
@@ -13,7 +13,7 @@ import com.eager.questioncloud.core.exception.CoreException
 import com.eager.questioncloud.core.exception.Error
 import com.eager.questioncloud.social.SocialAPIManager
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito
 import org.mockito.kotlin.any
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -35,10 +34,13 @@ internal class AuthenticationProcessorTest {
     @MockBean
     private val socialAPIManager: SocialAPIManager? = null
 
+    @AfterEach
+    fun tearDown() {
+        userRepository!!.deleteAllInBatch()
+    }
+
     @Test
-    @DisplayName("이메일과 비밀번호가 일치하고 Active 유저라면 유저 인증에 성공한다.")
-    @Transactional
-    fun 이메일과_비밀번호가_일치하고_Active_유저라면_유저_인증에_성공한다() {
+    fun 이메일과_비밀번호가_일치하고_Active_유저라면_유저_객체를_반환한다() {
         //given
         val email = "test@test.com"
         val password = "qwer1234"
@@ -63,8 +65,6 @@ internal class AuthenticationProcessorTest {
     }
 
     @Test
-    @DisplayName("존재하지 않는 이메일이면 로그인 실패 예외를 던진다.")
-    @Transactional
     fun 존재하지_않는_이메일이면_로그인_실패_예외를_던진다() {
         //given
         val wrongEmail = "test@test.com"
@@ -82,8 +82,6 @@ internal class AuthenticationProcessorTest {
     }
 
     @Test
-    @DisplayName("비밀번호가 일치하지 않으면 로그인 실패 예외를 던진다.")
-    @Transactional
     fun 비밀번호가_일치하지_않으면_로그인_실패_예외를_던진다() {
         //given
         val email = "test@test.com"
@@ -114,8 +112,6 @@ internal class AuthenticationProcessorTest {
     }
 
     @Test
-    @DisplayName("이메일 로그인 시 Active 상태가 아니라면 예외를 던진다.")
-    @Transactional
     fun 이메일_로그인_시_Active_상태가_아니라면_예외를_던진다() {
         //given
         val email = "test@test.com"
@@ -137,8 +133,6 @@ internal class AuthenticationProcessorTest {
     }
 
     @Test
-    @DisplayName("가입된 소셜 계정이라면 User가 담긴 SocialAuthentication을 반환한다.")
-    @Transactional
     fun 가입된_소셜_계정이라면_User가_담긴_SocialAuthentication을_반환한다() {
         //given
         val email = "test@test.com"
@@ -174,8 +168,6 @@ internal class AuthenticationProcessorTest {
     }
 
     @Test
-    @DisplayName("미가입 소셜 계정이라면 socialAccessToken이 담긴 SocialAuthentication을 반환한다.")
-    @Transactional
     fun 미가입_소셜_계정이라면_socialAccessToken이_담긴_SocialAuthentication을_반환한다() {
         //given
         val email = "test@test.com"
@@ -204,7 +196,6 @@ internal class AuthenticationProcessorTest {
     }
 
     @Test
-    @DisplayName("올바르지 않은 social code가 전달되면 소셜 로그인 실패 예외를 던진다.")
     fun 올바르지_않은_social_code가_전달되면_소셜_로그인_실패_예외를_던진다() {
         //given
         val wrongCode = "socialCode"
