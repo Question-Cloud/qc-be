bc91e889c2c38ceac00a07468c9a15f2f17a7f0e
e4g3r <qaws5656@naver.com>
2025-06-03 00:35:34 +0900

    refactor: NonCoreExceptionHandler -> NonCoreExceptionResolver  - HandlerExceptionResolver를 직접 구현하여 최종적으로 처리 되지 못한 예외들을 Slack 알림으로 전송하도록 변경

---
+++ qc-application/src/main/kotlin/com/eager/questioncloud/application/exception/NonCoreExceptionResolver.kt
@@ -0,0 +1,43 @@
+package com.eager.questioncloud.application.exception
+
+import com.eager.ApiTransactionContextHolder
+import com.eager.questioncloud.core.exception.Error
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerExceptionResolver
+import org.springframework.web.servlet.ModelAndView
+
+@Component
+@Order(Ordered.LOWEST_PRECEDENCE)
+class NonCoreExceptionResolver(private val slackNotifier: ExceptionSlackNotifier) : HandlerExceptionResolver {
+    private val cacheResponse =
+        ErrorResponse(Error.INTERNAL_SERVER_ERROR.httpStatus, Error.INTERNAL_SERVER_ERROR.message)
+    private val objectMapper = ObjectMapper().registerKotlinModule()
+
+    override fun resolveException(
+        req: HttpServletRequest,
+        res: HttpServletResponse,
+        handler: Any?,
+        ex: Exception
+    ): ModelAndView {
+        val transactionId = ApiTransactionContextHolder.get().transactionId
+        slackNotifier.sendApiException(ex, transactionId, req.requestURI, req.method)
+        ApiTransactionContextHolder.markException()
+        writeResponse(res)
+        return ModelAndView()
+    }
+    
+    private fun writeResponse(response: HttpServletResponse) {
+        response.status = HttpStatus.INTERNAL_SERVER_ERROR.value()
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+        response.writer.write(objectMapper.writeValueAsString(cacheResponse))
+    }
+}
