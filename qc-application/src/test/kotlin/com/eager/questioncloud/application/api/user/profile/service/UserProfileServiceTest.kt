3b8d3e706b952c6e1110fc2f780bff37098e022b
e4g3r <qaws5656@naver.com>
2025-06-08 20:50:32 +0900

    test: qc-application UserProfile 도메인 테스트 코드

---
+++ qc-application/src/test/kotlin/com/eager/questioncloud/application/api/user/profile/service/UserProfileServiceTest.kt
@@ -0,0 +1,48 @@
+package com.eager.questioncloud.application.api.user.profile.service
+
+import com.eager.questioncloud.application.utils.DBCleaner
+import com.eager.questioncloud.application.utils.fixture.helper.UserFixtureHelper
+import com.eager.questioncloud.core.domain.user.enums.UserStatus
+import com.eager.questioncloud.core.domain.user.infrastructure.repository.UserRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserProfileServiceTest(
+    @Autowired val userProfileService: UserProfileService,
+    @Autowired val userRepository: UserRepository,
+    @Autowired val dbCleaner: DBCleaner
+) {
+    @AfterEach
+    fun tearDown() {
+        dbCleaner.cleanUp()
+    }
+
+    @Test
+    fun `사용자의 이름과 프로필 이미지를 업데이트할 수 있다`() {
+        // given
+        val user = UserFixtureHelper.createEmailUser(
+            "test@example.com",
+            "password123",
+            UserStatus.Active,
+            userRepository
+        )
+
+        val newName = "업데이트된 이름"
+        val newProfileImage = "https://example.com/new-profile.jpg"
+
+        // when
+        userProfileService.updateUserInformation(user, newName, newProfileImage)
+
+        // then
+        val updatedUser = userRepository.getUser(user.uid)
+
+        assertThat(updatedUser.userInformation.name).isEqualTo(newName)
+        assertThat(updatedUser.userInformation.profileImage).isEqualTo(newProfileImage)
+    }
+}
