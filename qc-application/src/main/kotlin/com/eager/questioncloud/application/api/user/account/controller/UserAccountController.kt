e945a02c7239de6baca23163cb313a6698e9f916
e4g3r <qaws5656@naver.com>
2025-06-11 20:31:18 +0900

    doc: UserAccountController Document

--- qc-application/src/main/kotlin/com/eager/questioncloud/application/api/user/account/controller/UserAccountController.kt
+++ qc-application/src/main/kotlin/com/eager/questioncloud/application/api/user/account/controller/UserAccountController.kt
@@ -6,9 +6,6 @@ import com.eager.questioncloud.application.api.user.account.dto.ChangePasswordRe
 import com.eager.questioncloud.application.api.user.account.dto.RecoverForgottenEmailResponse
 import com.eager.questioncloud.application.api.user.account.service.UserAccountService
 import com.eager.questioncloud.application.security.UserPrincipal
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
@@ -19,16 +16,12 @@ class UserAccountController(
     private val userAccountService: UserAccountService
 ) {
     @GetMapping("/recover/email")
-    @ApiResponses(value = [ApiResponse(responseCode = "200", description = "요청 성공")])
-    @Operation(operationId = "계정 찾기", summary = "계정 찾기", tags = ["help"], description = "계정 찾기")
     fun recoverForgottenEmail(@RequestParam phone: String): RecoverForgottenEmailResponse {
         val email = userAccountService.recoverForgottenEmail(phone)
         return RecoverForgottenEmailResponse(email)
     }
 
     @GetMapping("/recover/password")
-    @ApiResponses(value = [ApiResponse(responseCode = "200", description = "요청 성공")])
-    @Operation(operationId = "비밀번호 찾기", summary = "비밀번호 찾기", tags = ["help"], description = "비밀번호 찾기")
     fun sendRecoverForgottenPasswordMail(@RequestParam email: String): DefaultResponse {
         userAccountService.sendRecoverForgottenPasswordMail(email)
         return success()
