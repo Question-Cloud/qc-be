72246c695b8b49f899b4c7239f85b623a2bedde2
e4g3r <qaws5656@naver.com>
2025-07-13 19:27:30 +0900

    fix: LockManager runCatching 문법 오류 수정

--- qc-lock-manager/src/main/kotlin/com/eager/questioncloud/lock/LockManager.kt
+++ qc-lock-manager/src/main/kotlin/com/eager/questioncloud/lock/LockManager.kt
@@ -9,26 +9,24 @@ import java.util.concurrent.TimeUnit
 class LockManager(private val redissonClient: RedissonClient) {
     private val WAIT_TIME = 3L
     private val LEASE_TIME = 5L
-
+    
     fun executeWithLock(key: String, task: Runnable) {
         val lockKey = LOCK_PREFIX + key
         val lock = redissonClient.getLock(lockKey)
-
+        
         runCatching {
             lock.tryLock(WAIT_TIME, LEASE_TIME, TimeUnit.SECONDS)
             if (!lock.isLocked) {
                 throw LockException()
             }
             task.run()
-        }.onFailure {
-            throw it
         }.also {
             if (lock.isLocked && lock.isHeldByCurrentThread) {
                 lock.unlock()
             }
-        }
+        }.getOrThrow()
     }
-
+    
     companion object {
         private const val LOCK_PREFIX = "LOCK:"
     }
