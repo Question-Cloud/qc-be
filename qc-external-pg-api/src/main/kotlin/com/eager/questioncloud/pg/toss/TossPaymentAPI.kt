3dca5dc918f7074797c871340ceb310f60904393
e4g3r <qaws5656@naver.com>
2025-07-17 15:18:24 +0900

    feat: pg-api 모듈 OkHttpClient timeout 수정

--- qc-external-pg-api/src/main/kotlin/com/eager/questioncloud/pg/toss/TossPaymentAPI.kt
+++ qc-external-pg-api/src/main/kotlin/com/eager/questioncloud/pg/toss/TossPaymentAPI.kt
@@ -18,20 +18,20 @@ import java.util.concurrent.TimeUnit
 class TossPaymentAPI {
     @Value("\${TOSS_SECRET_KEY}")
     private lateinit var TOSS_SECRET_KEY: String
-
+    
     fun getPayment(orderId: String): TossPayment {
         val request = Request.Builder()
             .url("${BASE_URL}/payments/orders/$orderId")
             .header("Authorization", "Basic $TOSS_SECRET_KEY")
             .get()
             .build()
-
+        
         client.newCall(request).execute().use { response ->
             validatePaymentResponse(response)
             return objectMapper.readValue(response.body!!.string(), TossPayment::class.java)
         }
     }
-
+    
     fun confirm(paymentId: String, orderId: String, amount: Int) {
         val request = Request.Builder()
             .url("${BASE_URL}/payments/confirm")
@@ -39,12 +39,12 @@ class TossPaymentAPI {
             .header("Idempotency-Key", paymentId)
             .post(getConfirmRequestBody(TossPaymentConfirmRequest(paymentId, orderId, amount)))
             .build()
-
+        
         client.newCall(request).execute().use { response ->
             validatePaymentResponse(response)
         }
     }
-
+    
     fun cancel(paymentId: String, amount: Int) {
         val request = Request.Builder()
             .url("${BASE_URL}/payments/$paymentId/cancel")
@@ -52,40 +52,41 @@ class TossPaymentAPI {
             .header("Idempotency-Key", paymentId)
             .post(getCancelRequestBody(TossPaymentCancelRequest(amount)))
             .build()
-
+        
         client.newCall(request).execute().use { response ->
             validatePaymentResponse(response)
         }
     }
-
+    
     private fun getConfirmRequestBody(request: TossPaymentConfirmRequest): RequestBody {
         return objectMapper.writeValueAsString(request).toRequestBody("application/json".toMediaType())
     }
-
+    
     private fun getCancelRequestBody(request: TossPaymentCancelRequest): RequestBody {
         return objectMapper.writeValueAsString(request).toRequestBody("application/json".toMediaType())
     }
-
+    
     private fun validatePaymentResponse(response: Response) {
         if (response.code == 200) {
             return
         }
-
+        
         if (response.code == 404) {
             throw CoreException(Error.INVALID_CHARGE_POINT_PAYMENT)
         }
-
+        
         if (response.body == null) {
             throw CoreException(Error.INVALID_CHARGE_POINT_PAYMENT)
         }
     }
-
+    
     companion object {
         private const val BASE_URL = "https://api.tosspayments.com/v1"
         private val objectMapper = ObjectMapper().registerKotlinModule()
         private val client = OkHttpClient().newBuilder()
-            .callTimeout(5, TimeUnit.SECONDS)
-            .readTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .callTimeout(15, TimeUnit.SECONDS)
             .build()
     }
 }
