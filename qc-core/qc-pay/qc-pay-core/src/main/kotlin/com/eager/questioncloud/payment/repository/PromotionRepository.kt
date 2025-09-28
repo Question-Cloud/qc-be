67d315e1b287187e93b0be9988ee0a0fbbb46843
e4g3r <qaws5656@naver.com>
2025-09-28 18:13:35 +0900

    test: QuestionOrderGeneratorTest 테스트 케이스 추가  - 프로모션 중인 문제 주문 생성 테스트 코드

--- qc-core/qc-pay/qc-pay-core/src/main/kotlin/com/eager/questioncloud/payment/repository/PromotionRepository.kt
+++ qc-core/qc-pay/qc-pay-core/src/main/kotlin/com/eager/questioncloud/payment/repository/PromotionRepository.kt
@@ -3,5 +3,7 @@ package com.eager.questioncloud.payment.repository
 import com.eager.questioncloud.payment.domain.Promotion
 
 interface PromotionRepository {
+    fun save(promotion: Promotion): Promotion
+    
     fun findByQuestionIdIn(questionIds: List<Long>): List<Promotion>
 }
--- qc-core/qc-pay/qc-pay-rdb/src/main/kotlin/com/eager/questioncloud/payment/repository/PromotionRepositoryImpl.kt
+++ qc-core/qc-pay/qc-pay-rdb/src/main/kotlin/com/eager/questioncloud/payment/repository/PromotionRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.eager.questioncloud.payment.repository
 
 import com.eager.questioncloud.payment.domain.Promotion
+import com.eager.questioncloud.payment.entity.PromotionEntity
 import com.eager.questioncloud.payment.entity.QPromotionEntity.promotionEntity
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
@@ -10,6 +11,10 @@ class PromotionRepositoryImpl(
     private val jpaQueryFactory: JPAQueryFactory,
     private val promotionJpaRepository: PromotionJpaRepository
 ) : PromotionRepository {
+    override fun save(promotion: Promotion): Promotion {
+        return promotionJpaRepository.save(PromotionEntity.from(promotion)).toModel()
+    }
+    
     override fun findByQuestionIdIn(questionIds: List<Long>): List<Promotion> {
         return jpaQueryFactory.select(promotionEntity)
             .from(promotionEntity)
