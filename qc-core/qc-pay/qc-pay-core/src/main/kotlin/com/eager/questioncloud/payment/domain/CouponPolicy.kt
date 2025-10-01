edd58d189393c72f72fcdceb028c8db374d5aaf2
e4g3r <qaws5656@naver.com>
2025-10-01 16:00:31 +0900

    refactor: DiscountCalculatePolicy 추상화 제거  - 쿠폰은 아직까지 단순히 고정 할인, 퍼센트 할인이기에 나중에 복잡해지면 다시 추상화 도입

--- qc-core/qc-pay/qc-pay-core/src/main/kotlin/com/eager/questioncloud/payment/domain/CouponPolicy.kt
+++ qc-core/qc-pay/qc-pay-core/src/main/kotlin/com/eager/questioncloud/payment/domain/CouponPolicy.kt
@@ -3,23 +3,24 @@ package com.eager.questioncloud.payment.domain
 import com.eager.questioncloud.common.exception.CoreException
 import com.eager.questioncloud.common.exception.Error
 import com.eager.questioncloud.payment.enums.DiscountCalculationType
+import kotlin.math.floor
 import kotlin.math.min
 
 class CouponPolicy(
     val coupon: Coupon,
     private val userCoupon: UserCoupon,
 ) : Discountable {
-    private val discountCalculatePolicy: DiscountCalculatePolicy = when (coupon.discountCalculationType) {
-        DiscountCalculationType.FIXED -> FixedDiscountCalculatePolicy(coupon.value)
-        DiscountCalculationType.PERCENT -> PercentDiscountCalculatePolicy(coupon.value)
-    }
-    
     override fun getDiscountAmount(originAmount: Int): Int {
         if (originAmount < coupon.minimumPurchaseAmount) {
             throw CoreException(Error.WRONG_COUPON)
         }
         
-        return min(discountCalculatePolicy.calculateDiscountAmount(originAmount), coupon.maximumDiscountAmount)
+        val discountAmount = when (coupon.discountCalculationType) {
+            DiscountCalculationType.FIXED -> coupon.value
+            DiscountCalculationType.PERCENT -> floor(originAmount * coupon.value / 100.0).toInt()
+        }
+        
+        return min(discountAmount, coupon.maximumDiscountAmount)
     }
     
     override fun getName(): String {
