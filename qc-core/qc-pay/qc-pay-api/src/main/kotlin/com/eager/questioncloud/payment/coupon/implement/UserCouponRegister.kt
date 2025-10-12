a5bb7aff589a555be0ff6802219a4e65759ab91b
e4g3r <qaws5656@naver.com>
2025-10-12 22:47:42 +0900

    refactor: 쿠폰 등록 Lock 관리 UserCouponRegister에서 처리하도록 변경

--- qc-core/qc-pay/qc-pay-api/src/main/kotlin/com/eager/questioncloud/payment/coupon/implement/UserCouponRegister.kt
+++ qc-core/qc-pay/qc-pay-api/src/main/kotlin/com/eager/questioncloud/payment/coupon/implement/UserCouponRegister.kt
@@ -2,6 +2,8 @@ package com.eager.questioncloud.payment.coupon.implement
 
 import com.eager.questioncloud.common.exception.CoreException
 import com.eager.questioncloud.common.exception.Error
+import com.eager.questioncloud.lock.LockKeyGenerator.generateRegisterCouponKey
+import com.eager.questioncloud.lock.LockManager
 import com.eager.questioncloud.payment.domain.UserCoupon
 import com.eager.questioncloud.payment.repository.CouponInformationRepository
 import com.eager.questioncloud.payment.repository.UserCouponRepository
@@ -12,19 +14,24 @@ import org.springframework.transaction.annotation.Transactional
 class UserCouponRegister(
     private val couponInformationRepository: CouponInformationRepository,
     private val userCouponRepository: UserCouponRepository,
+    private val lockManager: LockManager,
 ) {
     @Transactional
     fun registerCoupon(userId: Long, couponCode: String) {
-        val coupon = couponInformationRepository.findByCode(couponCode)
-        
-        if (userCouponRepository.isRegistered(userId, coupon.id)) {
-            throw CoreException(Error.ALREADY_REGISTER_COUPON)
+        lockManager.executeWithLock(
+            generateRegisterCouponKey(userId, couponCode)
+        ) {
+            val coupon = couponInformationRepository.findByCode(couponCode)
+            
+            if (userCouponRepository.isRegistered(userId, coupon.id)) {
+                throw CoreException(Error.ALREADY_REGISTER_COUPON)
+            }
+            
+            if (!couponInformationRepository.decreaseCount(coupon.id)) {
+                throw CoreException(Error.LIMITED_COUPON)
+            }
+            
+            userCouponRepository.save(UserCoupon.create(userId, coupon))
         }
-        
-        if (!couponInformationRepository.decreaseCount(coupon.id)) {
-            throw CoreException(Error.LIMITED_COUPON)
-        }
-        
-        userCouponRepository.save(UserCoupon.create(userId, coupon))
     }
 }
--- qc-core/qc-pay/qc-pay-api/src/main/kotlin/com/eager/questioncloud/payment/coupon/service/UserCouponService.kt
+++ qc-core/qc-pay/qc-pay-api/src/main/kotlin/com/eager/questioncloud/payment/coupon/service/UserCouponService.kt
@@ -1,7 +1,5 @@
 package com.eager.questioncloud.payment.coupon.service
 
-import com.eager.questioncloud.lock.LockKeyGenerator.generateRegisterCouponKey
-import com.eager.questioncloud.lock.LockManager
 import com.eager.questioncloud.payment.coupon.implement.UserCouponRegister
 import com.eager.questioncloud.payment.dto.AvailableUserCoupon
 import com.eager.questioncloud.payment.repository.UserCouponRepository
@@ -11,12 +9,9 @@ import org.springframework.stereotype.Service
 class UserCouponService(
     private val userCouponRegister: UserCouponRegister,
     private val userCouponRepository: UserCouponRepository,
-    private val lockManager: LockManager,
 ) {
     fun registerCoupon(userId: Long, couponCode: String) {
-        lockManager.executeWithLock(
-            generateRegisterCouponKey(userId, couponCode)
-        ) { userCouponRegister.registerCoupon(userId, couponCode) }
+        userCouponRegister.registerCoupon(userId, couponCode)
     }
     
     fun getAvailableUserCoupons(userId: Long): List<AvailableUserCoupon> {
